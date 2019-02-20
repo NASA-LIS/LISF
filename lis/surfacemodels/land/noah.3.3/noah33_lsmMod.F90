ca42498d7b7251e40dd00325afebd37224fe4987
Daniel Rosen <daniel.rosen@noaa.gov>
2019-02-20 12:01:34 -0700

    Initialize LISWRF data structure with undefined value. Initialize smc, stc, relsmc to undefined value in Noah.3.3 lsm.

--- lis/surfacemodels/land/noah.3.3/noah33_lsmMod.F90
+++ lis/surfacemodels/land/noah.3.3/noah33_lsmMod.F90
@@ -216,6 +216,11 @@ contains
           noah33_struc(n)%noah(i)%shdfac = 0 
           noah33_struc(n)%noah(i)%alb = 0 
           noah33_struc(n)%noah(i)%z0 = 0 
+
+          noah33_struc(n)%noah(i)%stc = LIS_rc%udef
+          noah33_struc(n)%noah(i)%smc = LIS_rc%udef
+          noah33_struc(n)%noah(i)%sh2o = LIS_rc%udef
+          noah33_struc(n)%noah(i)%relsmc = LIS_rc%udef
 #if WRF_HYDRO
           noah33_struc(n)%noah(i)%sfhead1rt = 0
           noah33_struc(n)%noah(i)%infxs1rt = 0
