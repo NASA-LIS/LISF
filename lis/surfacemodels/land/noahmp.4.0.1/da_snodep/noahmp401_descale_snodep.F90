3efc20d41c5131a9603f6f0afdf9f76a46652ae4
Yeosang Yoon <yeosang.yoon@nasa.gov>
2019-05-17 11:19:12 -0400

    Add SNODEP DA support to NoahMP 4.0.1

---
+++ lis/surfacemodels/land/noahmp.4.0.1/da_snodep/noahmp401_descale_snodep.F90
@@ -0,0 +1,70 @@
+!-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+! NASA Goddard Space Flight Center Land Information System (LIS) v7.2
+!
+! Copyright (c) 2015 United States Government as represented by the
+! Administrator of the National Aeronautics and Space Administration.
+! All Rights Reserved.
+!-------------------------END NOTICE -- DO NOT EDIT-----------------------
+!BOP
+! !ROUTINE: noahmp401_descale_snodep
+! \label{noahmp401_descale_snodep}
+!
+! !REVISION HISTORY:
+! 27Feb2005: Sujay Kumar; Initial Specification
+! 25Jun2006: Sujay Kumar: Updated for the ESMF design
+! 02 Mar 2010: Sujay Kumar; Modified for Noah 3.1
+! 14 Dec 2018: Yeosang Yoon; Modified for NoahMP 4.0.1
+!
+! !INTERFACE:
+subroutine noahmp401_descale_snodep(n, LSM_State, LSM_Incr_State)
+
+! !USES:
+  use ESMF
+  use LIS_coreMod, only : LIS_rc
+  use noahmp401_lsmMod
+  use LIS_logMod
+
+  implicit none
+! !ARGUMENTS: 
+  integer, intent(in)    :: n
+  type(ESMF_State)       :: LSM_State
+  type(ESMF_State)       :: LSM_Incr_State
+!
+! !DESCRIPTION:
+!
+!  Returns the snow related state prognostic variables for
+!  data assimilation
+! 
+!  The arguments are: 
+!  \begin{description}
+!  \item[n] index of the nest \newline
+!  \item[LSM\_State] ESMF State container for LSM state variables \newline
+!  \end{description}
+!EOP
+
+  type(ESMF_Field)       :: sweField
+  type(ESMF_Field)       :: snodField
+
+  integer                :: t
+  integer                :: status
+  real, pointer          :: swe(:)
+  real, pointer          :: snod(:)
+ 
+#if 0
+  call ESMF_StateGet(LSM_State,"SWE",sweField,rc=status)
+  call LIS_verify(status)
+  call ESMF_StateGet(LSM_State,"Snowdepth",snodField,rc=status)
+  call LIS_verify(status)
+
+  call ESMF_FieldGet(sweField,localDE=0,farrayPtr=swe,rc=status)
+  call LIS_verify(status)
+  call ESMF_FieldGet(snodField,localDE=0,farrayPtr=snod,rc=status)
+  call LIS_verify(status)
+
+  do t=1,LIS_rc%npatch(n,LIS_rc%lsm_index)
+     swe(t) = swe(t)*1000.0
+  enddo
+#endif
+
+end subroutine noahmp401_descale_snodep
+
---
+++ lis/surfacemodels/land/noahmp.4.0.1/da_snodep/noahmp401_transform_snodep.F90
@@ -0,0 +1,72 @@
+!-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+! NASA Goddard Space Flight Center Land Information System (LIS) v7.2
+!
+! Copyright (c) 2015 United States Government as represented by the
+! Administrator of the National Aeronautics and Space Administration.
+! All Rights Reserved.
+!-------------------------END NOTICE -- DO NOT EDIT-----------------------
+!BOP
+! !ROUTINE: noahmp401_transform_snodep
+! \label{noahmp401_transform_snodep}
+!
+! !REVISION HISTORY:
+! 25Jun2006: Sujay Kumar: Initial Specification
+!  02 Mar 2010: Sujay Kumar; Modified for Noah 3.1
+!  21 Jul 2011: James Geiger; Modified for Noah 3.2
+!  03 Oct 2018; Yeosang Yoon; Modified for NoahMP 3.6
+!  14 Dec 2018: Yeosang Yoon; Modified for NoahMP 4.0.1
+!
+! !INTERFACE:
+subroutine noahmp401_transform_snodep(n,OBS_State)
+
+! !USES:
+  use ESMF
+  use LIS_coreMod, only : LIS_rc
+  use LIS_logMod,  only : LIS_verify
+  use noahmp401_lsmMod
+  use SNODEPobs_Mod, only : SNODEP_obs_obj
+!EOP
+  implicit none
+
+  integer, intent(in)      :: n
+  type(ESMF_State)         :: OBS_State
+!
+! !DESCRIPTION:
+!
+!  This subroutine transforms the SNODEP state
+!  (mm) to the lsm state
+!  
+!  The arguments are: 
+!  \begin{description}
+!  \item[n] index of the nest \newline
+!  \item[OBS\_State] ESMF state container for observations \newline
+!  \end{description}
+!EOP
+  type(ESMF_Field)         :: obs_snodep_field
+  real, pointer            :: snodepobs(:)
+  integer                  :: t
+  integer                  :: N_obs_size
+  integer                  :: status
+  
+  call ESMF_AttributeGet(OBS_State,name="Number Of Observations",&
+       value=N_obs_size,rc=status)
+  call LIS_verify(status, 'attributeget error in noahmp401_transform_snodep')
+  call ESMF_StateGet(OBS_State,"Observation01",obs_snodep_field,&
+       rc=status)
+  call LIS_verify(status,'stateget error in noahmp401_transform_snodep')
+  call ESMF_FieldGet(obs_snodep_field,localDE=0,farrayPtr=snodepobs,rc=status)
+  call LIS_verify(status,'fieldget error in noahmp401_transform_snodep')
+
+  ! If using 8th mesh SNODEP data, convert it from inches to meters.
+  ! 16th mesh SNODEP data are already in meters.
+  if ( SNODEP_obs_obj(n)%mesh == 8 ) then
+     do t=1,N_obs_size
+        if(snodepobs(t).ne.LIS_rc%udef) then 
+           if(snodepobs(t).gt.408.0) then 
+              snodepobs(t) = 0.0        
+           endif
+           snodepobs(t) = snodepobs(t)*2.54E-2     !inches to meters
+        endif
+     enddo
+  endif
+end subroutine noahmp401_transform_snodep
