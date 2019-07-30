725fb1c97cdb111a8a0701244f9b8e50e4e74ce3
David Mocko <38500701+dmocko@users.noreply.github.com>
2019-07-30 14:57:49 -0400

    Update Config.pl
    Added spaces at the end of $sys_opt lines for optimization level -3.
    Otherwise, the configure.lvt file will have compiler options not separated by a space.

--- lvt/arch/Config.pl
+++ lvt/arch/Config.pl
@@ -82,7 +82,7 @@ if($opt_lev == -3) {
 	$sys_opt = "-g -warn";
 	$sys_opt .= 
            " -check bounds,format,output_conversion,pointers,stack,uninit";
-	$sys_opt .= " -fp-stack-check -ftrapuv";
+	$sys_opt .= " -fp-stack-check -ftrapuv ";
 
 	$sys_c_opt = "-g -Wall -Wcast-qual -Wcheck -Wdeprecated";
 	$sys_c_opt .= " -Wextra-tokens -Wformat";
@@ -100,12 +100,12 @@ if($opt_lev == -3) {
     elsif($sys_arch eq "linux_pgi") {
 	print "Optimization level $opt_lev is not defined for $sys_arch.\n";
 	print "Using '-g'\n";
-	$sys_opt = "-g";
+	$sys_opt = "-g ";
     }
     elsif($sys_arch eq "linux_absoft") {
 	print "Optimization level $opt_lev is not defined for $sys_arch.\n";
 	print "Using '-g'\n";
-	$sys_opt = "-g";
+	$sys_opt = "-g ";
     }
     elsif($sys_arch eq "linux_lf95") {
 	print "Optimization level $opt_lev is not defined for $sys_arch.\n";
@@ -117,7 +117,7 @@ if($opt_lev == -3) {
 	$sys_opt .= " -Wrealloc-lhs-all";
 	# Run-time options
 	$sys_opt .= " -ffpe-trap=invalid,zero,overflow";
-	$sys_opt .= " -fcheck=all,no-array-temps";
+	$sys_opt .= " -fcheck=all,no-array-temps ";
 
 	$sys_c_opt = "-g -Wall -Wextra -Wpedantic -Wformat -Wtraditional";
 	$sys_c_opt .= " -Wconversion";
@@ -127,13 +127,13 @@ if($opt_lev == -3) {
     elsif($sys_arch eq "AIX") {
 	print "Optimization level $opt_lev is not defined for $sys_arch.\n";
 	print "Using '-g'\n";
-	$sys_opt = "-g";
+	$sys_opt = "-g ";
     }
 }
 
 
 if($opt_lev == -2) {
-   $sys_opt = "-g -check bounds,format,output_conversion,pointers,stack,uninit  -fp-stack-check -ftrapuv ";
+   $sys_opt = "-g -check bounds,format,output_conversion,pointers,stack,uninit -fp-stack-check -ftrapuv ";
    $sys_c_opt = "-g ";
 }
 if($opt_lev == -1) {
