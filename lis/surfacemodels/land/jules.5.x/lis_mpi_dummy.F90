d077a38c50613c708090caf3a3eefbd4dca02e60
Shugong Wang <shugong.wang@nasa.gov>
2019-12-18 15:19:39 -0500

    Clean coppy right descriptions for the two files

--- lis/surfacemodels/land/jules.5.x/lis_mpi_dummy.F90
+++ lis/surfacemodels/land/jules.5.x/lis_mpi_dummy.F90
@@ -1,86 +1,43 @@
 ! 07/12/2019 Shugong Wang for Jules 5.5 for disabling MPI calls in JULES code
 ! The code is based on the dummy MPI code of JULES
-! *****************************COPYRIGHT**************************************
-! (C) Crown copyright Met Office. All rights reserved.
-! For further details please refer to the file COPYRIGHT.txt
-! which you should have received as part of this distribution.
-! *****************************COPYRIGHT**************************************
-
 MODULE lis_mpi_dummy
-
   IMPLICIT NONE
-
-!-----------------------------------------------------------------------------
-! Description:
-!   This module contains just the variable definitions from a "real" mpi
-!   module that JULES needs to compile against
-!
-! Current Code Owner: Kerry Smout-Day
-!
-! Code Description:
-!   Language: Fortran 90.
-!   This code is written to JULES coding standards v1.
-!-----------------------------------------------------------------------------
-
   INTEGER, PARAMETER :: mpi_comm_world = 1
   INTEGER, PARAMETER :: mpi_real = 1
   INTEGER, PARAMETER :: mpi_integer = 1
   INTEGER, PARAMETER :: mpi_logical = 1
   INTEGER, PARAMETER :: mpi_info_null = 1
   INTEGER, PARAMETER :: mpi_land = 1
-
-! Setting this to 1 means use the default INTEGER size
   INTEGER, PARAMETER :: mpi_address_kind = 1
   interface mpi_bcast
     procedure mpi_bcast1, mpi_bcast2, mpi_bcast3, mpi_bcast4, mpi_bcast5
   end interface
-
   interface mpi_type_create_resized
     procedure mpi_type_create_resized1, mpi_type_create_resized2
   end interface 
 contains
   subroutine mpi_init(error)
-
     implicit none
-
     integer, intent(out) :: error
-
   ! this procedure has nothing to do
-
     error = 0
-
     return
-
   end subroutine mpi_init
 
-
-
   subroutine mpi_finalize(error)
-
     implicit none
-
     integer, intent(out) :: error
-
   ! this procedure has nothing to do
-
     error = 0
-
     return
-
   end subroutine mpi_finalize
 
-
-
   subroutine mpi_abort(comm, errorcode, error)
-
     use, intrinsic :: iso_c_binding, only: c_int
-
     implicit none
-
     integer, intent(in) :: comm
     integer, intent(in) :: errorcode
     integer, intent(out) :: error
-
     ! all this procedure needs to do is terminate the current process
     ! with a non-zero exit code. we use the c intrinsic in order to
     ! ensure portability.
@@ -93,242 +50,158 @@ contains
     end interface
     error = 1
     call c_exit(1_c_int)
-
     return
-
   end subroutine mpi_abort
 
-
-
   subroutine mpi_comm_size(comm, size, error)
-
     implicit none
-
     integer, intent(in) :: comm
     integer, intent(out) :: size
     integer, intent(out) :: error
-
   ! in serial mode, the size is always 1
     size  = 1
     error = 0
-
     return
-
   end subroutine mpi_comm_size
 
-
-
   subroutine mpi_comm_rank(comm, rank, error)
-
     implicit none
-
     integer, intent(in) :: comm
     integer, intent(out) :: rank
     integer, intent(out) :: error
-
   ! in serial mode, the rank is always 0
     rank  = 0
     error = 0
-
     return
-
   end subroutine mpi_comm_rank
-
-
 
   subroutine mpi_scatterv(sendbuf, sendcnts, displs, sendtype,                  &
                           recvbuf, recvcnt, recvtype,                           &
                           root, comm, error)
-
     implicit none
-
     real, intent(in) :: sendbuf(:,:)
     integer, intent(in) :: sendcnts(:)
     integer, intent(in) :: displs(:)
     integer, intent(in) :: sendtype
-
     real, intent(out) :: recvbuf(:,:)
     integer, intent(in) :: recvcnt
     integer, intent(in) :: recvtype
-
     integer, intent(in) :: root
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! this routine ignores all count and offset information and just copies all
   ! the data from sendbuf into recvbuf
   ! at the moment, this routine only exists for 2d real arrays, which is all jules uses
   ! it for
-
   ! the input and output buffers must be the same size
     if ( size(sendbuf) /= size(recvbuf) ) then
       write(*,*) 'fatal error: attempt to use dummy mpi procedure for non-serial work'
       write(*,*) 'to use mpi routines for parallel execution, recompile using an mpi compiler'
-
       call mpi_abort(comm, 1, error)
     end if
-
   ! just copy the data from the input buffer to the output buffer
     recvbuf(:,:) = sendbuf(:,:)
-
     error = 0
-
     return
-
   end subroutine mpi_scatterv
-
-
 
   subroutine mpi_gatherv(sendbuf, sendcnt, sendtype,                             &
                          recvbuf, recvcnts, displs, recvtype,                    &
                          root, comm, error)
-
     implicit none
-
     real, intent(in) :: sendbuf(:,:)
     integer, intent(in) :: sendcnt
     integer, intent(in) :: sendtype
-
     real, intent(out) :: recvbuf(:,:)
     integer, intent(in) :: recvcnts(:)
     integer, intent(in) :: displs(:)
     integer, intent(in) :: recvtype
-
     integer, intent(in) :: root
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! this routine ignores all count and offset information and just copies all
   ! the data from sendbuf into recvbuf
   ! at the moment, this routine only exists for 2d real arrays, which is all jules uses
   ! it for
-
   ! the input and output buffers must be the same size
     if ( size(sendbuf) /= size(recvbuf) ) then
       write(*,*) 'fatal error: attempt to use dummy mpi procedure for non-serial work'
       write(*,*) 'to use mpi routines for parallel execution, recompile using an mpi compiler'
-
       call mpi_abort(comm, 1, error)
     end if
-
   ! just copy the data from the input buffer to the output buffer
     recvbuf(:,:) = sendbuf(:,:)
-
     error = 0
-
     return
-
   end subroutine mpi_gatherv
 
-
-
   subroutine mpi_bcast1(buffer, count, datatype, root, comm, error)
-
     implicit none
-
     integer, intent(in) :: count
     real, intent(inout) :: buffer(count)
     integer, intent(in) :: datatype
-
     integer, intent(in) :: root
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! since there is only one task, we don't need to do anything to broadcast a value
-
     error = 0
-
     return
-
   end subroutine mpi_bcast1
   
   subroutine mpi_bcast2(buffer, count, datatype, root, comm, error)
-
     implicit none
-
     integer, intent(in) :: count
     real, intent(inout) :: buffer
     integer, intent(in) :: datatype
-
     integer, intent(in) :: root
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! since there is only one task, we don't need to do anything to broadcast a value
-
     error = 0
-
     return
-
   end subroutine mpi_bcast2
   
   subroutine mpi_bcast3(buffer, count, datatype, root, comm, error)
-
     implicit none
-
     integer, intent(in) :: count
     logical, intent(inout) :: buffer
     integer, intent(in) :: datatype
-
     integer, intent(in) :: root
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! since there is only one task, we don't need to do anything to broadcast a value
-
     error = 0
-
     return
-
   end subroutine mpi_bcast3
   
   subroutine mpi_bcast4(buffer, count, datatype, root, comm, error)
-
     implicit none
-
     integer, intent(in) :: count
     integer, intent(in) :: buffer(count)
     integer, intent(in) :: datatype
-
     integer, intent(in) :: root
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! since there is only one task, we don't need to do anything to broadcast a value
-
     error = 0
-
     return
-
   end subroutine mpi_bcast4
   
   subroutine mpi_bcast5(buffer, count, datatype, root, comm, error)
-
     implicit none
-
     integer, intent(in) :: count
     integer, intent(in) :: buffer
     integer, intent(in) :: datatype
-
     integer, intent(in) :: root
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! since there is only one task, we don't need to do anything to broadcast a value
-
     error = 0
-
     return
-
   end subroutine mpi_bcast5
 
-
-
-
   subroutine mpi_allreduce(sendbuf, recvbuf, sendcnt, datatype, op, comm, error)
-
     implicit none
-
     logical, intent(in) :: sendbuf
     logical, intent(out) :: recvbuf
     integer, intent(in) :: sendcnt
@@ -336,135 +209,85 @@ contains
     integer, intent(in) :: op
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! this routine only exists in the dummy library for logical variables
   ! the op is ignored, and this is just treated like a bcast
     recvbuf = sendbuf
-
     error = 0
-
     return
-
   end subroutine mpi_allreduce
-
-
-
   subroutine mpi_barrier(comm, error)
-
     implicit none
-
     integer, intent(in) :: comm
     integer, intent(out) :: error
-
   ! since there is only one task, we don't need to do anything to form a barrier
-
     error = 0
-
     return
-
   end subroutine mpi_barrier
 
-
-
   subroutine mpi_type_get_extent(type, lb, extent, error)
-
     implicit none
-
     integer, intent(in) :: type
     integer(kind=mpi_address_kind), intent(out) :: lb
     integer(kind=mpi_address_kind), intent(out) :: extent
     integer, intent(out) :: error
-
   ! all information about mpi types is ignored by the dummy mpi routines, so
   ! it doesn't matter what we return
     lb     = 0
     extent = 4
     error  = 0
-
     return
-
   end subroutine mpi_type_get_extent
 
-
-
   subroutine mpi_type_vector(count, blocklength, stride, old_type, new_type, error)
-
     implicit none
-
     integer, intent(in) :: count
     integer, intent(in) :: blocklength
     integer, intent(in) :: stride
     integer, intent(in) :: old_type
     integer, intent(out) :: new_type
     integer, intent(out) :: error
-
   ! all information about mpi types is ignored by the dummy mpi routines, so
   ! it doesn't matter what we return
-
     new_type = 1
     error    = 0
-
     return
-
   end subroutine mpi_type_vector
 
-
-
   subroutine mpi_type_create_resized1(old_type, lb, extent, new_type, error)
-
     implicit none
-
     integer, intent(in) :: old_type
     integer, intent(in) :: lb
     integer, intent(in) :: extent
     integer, intent(out) :: new_type
     integer, intent(out) :: error
-
   ! all information about mpi types is ignored by the dummy mpi routines, so
   ! it doesn't matter what we return
-
     new_type = 1
     error    = 0
-
     return
-
   end subroutine mpi_type_create_resized1
 
   subroutine mpi_type_create_resized2(old_type, lb, extent, new_type, error)
-
     implicit none
-
     integer, intent(in) :: old_type
     integer(kind=mpi_address_kind) :: lb
     integer(kind=mpi_address_kind),intent(in) ::  extent
     integer, intent(out) :: new_type
     integer, intent(out) :: error
-
   ! all information about mpi types is ignored by the dummy mpi routines, so
   ! it doesn't matter what we return
-
     new_type = 1
     error    = 0
-
     return
-
   end subroutine mpi_type_create_resized2
 
-
   subroutine mpi_type_commit(datatype, error)
-
     implicit none
-
     integer, intent(in) :: datatype
     integer, intent(out) :: error
-
   ! all information about mpi types is ignored by the dummy mpi routines, so
   ! there is nothing to do
-
     error = 0
-
     return
-
   end subroutine mpi_type_commit
-
 END MODULE lis_mpi_dummy
--- lis/surfacemodels/land/jules.5.x/logging_mod.F90
+++ lis/surfacemodels/land/jules.5.x/logging_mod.F90
@@ -1,276 +1,75 @@
 !!! Shugong Wang, 07/12/2019 LIS version of logging_mod with write_to_log disabled
+!!! 
 #if !defined(UM_JULES)
-! *****************************COPYRIGHT**************************************
-! (C) Crown copyright Met Office. All rights reserved.
-! For further details please refer to the file COPYRIGHT.txt
-! which you should have received as part of this distribution.
-! *****************************COPYRIGHT**************************************
-
 
 MODULE logging_mod
-
 USE mpi
-
 USE iso_fortran_env, ONLY: output_unit, error_unit
-
 IMPLICIT NONE
 
-! Log levels - these can be combined using bitwise operators to indicate
-! any combination of log levels
 INTEGER, PARAMETER ::                                                         &
   log_level_info  = 1,                                                        &
   log_level_debug = 2,                                                        &
   log_level_warn  = 4,                                                        &
   log_level_error = 8,                                                        &
   log_level_fatal = 16
-
-! Determines what log levels are printed to log_unit - this is a bitwise
-! combination of values from above.
-! The default (31) is to print everything
 INTEGER, PARAMETER :: log_print_level = 31
-
-! Determines what log levels cause a program to stop - this is a bitwise
-! combination of values from above.
-! Fatal errors will always cause the program to stop, by definition.
-! The default (0) is to stop only for fatal errors
-! Setting this to 15 (i.e. stop for everything, even info) or 14 (i.e. stop
-! for everything except info) are useful options for debugging
 INTEGER, PARAMETER :: log_stop_level = 0
-
-! The maximum line length for log entries. Any message larger than this is
-! truncated
 INTEGER, PARAMETER :: log_max_line_len = 500
-
-! The calculated number of tasks id and task prefix
-! These are cached the first time they are calculated to avoid calling the
-! MPI routines every time a log message is written
 INTEGER :: ntasks = 0
 CHARACTER(LEN=20) :: task_prefix = ""
-
-
-! Visibilities
 PRIVATE
 PUBLIC :: log_info, log_debug, log_warn, log_error, log_fatal
 
 CONTAINS
-
 SUBROUTINE write_to_log(log_level, proc_name, message)
-
-#if defined(INTEL_FORTRAN)
-USE ifcore, ONLY: tracebackqq
-#endif
-
-USE string_utils_mod, ONLY: to_string
-
-!-----------------------------------------------------------------------------
-! Description:
-!   Logs the given message at the given level and performs any necessary
-!   action
-!
-! Code Owner: Please refer to ModuleLeaders.txt
-! This file belongs in TECHNICAL
-!
-! Code Description:
-!   Language: Fortran 90.
-!   This code is written to JULES coding standards v1.
-!-----------------------------------------------------------------------------
-! Argument types
-INTEGER, INTENT(IN) :: log_level
-  ! The level at which to log the given message
-CHARACTER(LEN=*), INTENT(IN) :: proc_name
-  ! The name of the originating routine/function
-CHARACTER(LEN=*), INTENT(IN) :: message
-  ! The message to log
-
-! Work variables
-CHARACTER(LEN=log_max_line_len) :: full_message
-INTEGER :: task_id, error
-
-!-----------------------------------------------------------------------------
-
-!-----------------------------------------------------------------------------
-! Construct the full log message
-!-----------------------------------------------------------------------------
-! Combine the name of the originating procedure with the message
-full_message = TRIM(proc_name) // ': ' // message
-
-! Prepend a fragment to the message depending on the log level
-SELECT CASE ( log_level )
-CASE ( log_level_info )
-  full_message = "[INFO] " // full_message
-
-CASE ( log_level_debug )
-  full_message = "[DEBUG] " // full_message
-
-CASE ( log_level_warn )
-  full_message = "[WARNING] " // full_message
-
-CASE ( log_level_error )
-  full_message = "[ERROR] " // full_message
-
-CASE ( log_level_fatal )
-  full_message = "[FATAL ERROR] " // full_message
-
-CASE DEFAULT
-  ! This should never happen since the only public access to write_to_log is
-  ! through the log_* routines defined below
-  CALL log_fatal("write_to_log", "Unknown log level")
-END SELECT
-
-!!! disabled by Shugong Wang 
-#if 0
-! Calculate the number of tasks and task prefix if we have not already
-IF ( ntasks <= 0 ) THEN
-  CALL mpi_comm_size(mpi_comm_world, ntasks, error)
-  CALL mpi_comm_rank(mpi_comm_world, task_id, error)
-  IF ( ntasks > 1 ) THEN
-    task_prefix = "{MPI Task " // TRIM(to_string(task_id)) // "}"
-  END IF
-END IF
-
-! Prepend the task prefix
-full_message = ADJUSTL(TRIM(task_prefix) // " " // full_message)
-
-!-----------------------------------------------------------------------------
-! Use a bitwise and to check if we want to print log messages for the
-! given log level
-!-----------------------------------------------------------------------------
-IF ( IAND(log_print_level, log_level) > 0 ) THEN
-  IF ( log_level <= log_level_debug ) THEN
-    ! Print info and debug to stdout
-    WRITE(output_unit, "(A)") TRIM(full_message)
-  ELSE
-    ! Print errors to stdout
-    WRITE(error_unit, "(A)") TRIM(full_message)
-  END IF
-END IF
-
-!-----------------------------------------------------------------------------
-! Check if we need to stop the program
-!-----------------------------------------------------------------------------
-IF ( IAND(log_stop_level, log_level) > 0 .OR.                                 &
-     log_level == log_level_fatal ) THEN
-  ! If stopping abnormally, attempt to print a stack trace
-#if defined(GNU_FORTRAN)
-  ! Currently, the default Met Office version of gfortran is too old to support
-  ! BACKTRACE, as is the default gfortran version in some common OS package
-  ! systems
-  ! We'll add this back in when gfortran 4.8 is fully available
-  !      CALL BACKTRACE()
-#elif defined(INTEL_FORTRAN)
-  CALL tracebackqq(user_exit_code = -1)
-#endif
-
-  ! Abort MPI with a non-zero exit code
-  CALL mpi_abort(mpi_comm_world, 1, error)
-END IF
-#endif
+  USE string_utils_mod, ONLY: to_string
+  INTEGER, INTENT(IN) :: log_level
+  CHARACTER(LEN=*), INTENT(IN) :: proc_name
+  CHARACTER(LEN=*), INTENT(IN) :: message
+  CHARACTER(LEN=log_max_line_len) :: full_message
+  INTEGER :: task_id, error
+  full_message = TRIM(proc_name) // ': ' // message
+  SELECT CASE ( log_level )
+  CASE ( log_level_info )
+    full_message = "[INFO] " // full_message
+  CASE ( log_level_debug )
+    full_message = "[DEBUG] " // full_message
+  CASE ( log_level_warn )
+    full_message = "[WARNING] " // full_message
+  CASE ( log_level_error )
+    full_message = "[ERROR] " // full_message
+  CASE ( log_level_fatal )
+    full_message = "[FATAL ERROR] " // full_message
+  CASE DEFAULT
+    CALL log_fatal("write_to_log", "Unknown log level")
+  END SELECT
 END SUBROUTINE write_to_log
 
-
 SUBROUTINE log_info(proc_name, message)
-
-!-----------------------------------------------------------------------------
-! Description:
-!   Utility function to write a message to the log at level info
-!
-! Code Owner: Please refer to ModuleLeaders.txt
-! This file belongs in TECHNICAL
-!
-! Code Description:
-!   Language: Fortran 90.
-!   This code is written to JULES coding standards v1.
-!-----------------------------------------------------------------------------
-CHARACTER(LEN=*), INTENT(IN) :: proc_name
-CHARACTER(LEN=*), INTENT(IN) :: message
-
-CALL write_to_log(log_level_info, proc_name, message)
-
+  CHARACTER(LEN=*), INTENT(IN) :: proc_name
+  CHARACTER(LEN=*), INTENT(IN) :: message
+  CALL write_to_log(log_level_info, proc_name, message)
 END SUBROUTINE log_info
-
-
 SUBROUTINE log_debug(proc_name, message)
-
-!-----------------------------------------------------------------------------
-! Description:
-!   Utility function to write a message to the log at level debug
-!
-! Code Owner: Please refer to ModuleLeaders.txt
-! This file belongs in TECHNICAL
-!
-! Code Description:
-!   Language: Fortran 90.
-!   This code is written to JULES coding standards v1.
-!-----------------------------------------------------------------------------
-CHARACTER(LEN=*), INTENT(IN) :: proc_name
-CHARACTER(LEN=*), INTENT(IN) :: message
-
-CALL write_to_log(log_level_debug, proc_name, message)
-
+  CHARACTER(LEN=*), INTENT(IN) :: proc_name
+  CHARACTER(LEN=*), INTENT(IN) :: message
+  CALL write_to_log(log_level_debug, proc_name, message)
 END SUBROUTINE log_debug
-
-
 SUBROUTINE log_warn(proc_name, message)
-
-!-----------------------------------------------------------------------------
-! Description:
-!   Utility function to write a message to the log at level warn
-!
-! Code Owner: Please refer to ModuleLeaders.txt
-! This file belongs in TECHNICAL
-!
-! Code Description:
-!   Language: Fortran 90.
-!   This code is written to JULES coding standards v1.
-!-----------------------------------------------------------------------------
-CHARACTER(LEN=*), INTENT(IN) :: proc_name
-CHARACTER(LEN=*), INTENT(IN) :: message
-
-CALL write_to_log(log_level_warn, proc_name, message)
-
+  CHARACTER(LEN=*), INTENT(IN) :: proc_name
+  CHARACTER(LEN=*), INTENT(IN) :: message
+  CALL write_to_log(log_level_warn, proc_name, message)
 END SUBROUTINE log_warn
-
-
 SUBROUTINE log_error(proc_name, message)
-
-!-----------------------------------------------------------------------------
-! Description:
-!   Utility function to write a message to the log at level error
-!
-! Code Owner: Please refer to ModuleLeaders.txt
-! This file belongs in TECHNICAL
-!
-! Code Description:
-!   Language: Fortran 90.
-!   This code is written to JULES coding standards v1.
-!-----------------------------------------------------------------------------
-CHARACTER(LEN=*), INTENT(IN) :: proc_name
-CHARACTER(LEN=*), INTENT(IN) :: message
-
-CALL write_to_log(log_level_error, proc_name, message)
-
+  CHARACTER(LEN=*), INTENT(IN) :: proc_name
+  CHARACTER(LEN=*), INTENT(IN) :: message
+  CALL write_to_log(log_level_error, proc_name, message)
 END SUBROUTINE log_error
-
-
 SUBROUTINE log_fatal(proc_name, message)
-
-!-----------------------------------------------------------------------------
-! Description:
-!   Utility function to write a message to the log at level fatal
-!
-! Code Owner: Please refer to ModuleLeaders.txt
-! This file belongs in TECHNICAL
-!
-! Code Description:
-!   Language: Fortran 90.
-!   This code is written to JULES coding standards v1.
-!-----------------------------------------------------------------------------
-CHARACTER(LEN=*), INTENT(IN) :: proc_name
-CHARACTER(LEN=*), INTENT(IN) :: message
-
-CALL write_to_log(log_level_fatal, proc_name, message)
-
+  CHARACTER(LEN=*), INTENT(IN) :: proc_name
+  CHARACTER(LEN=*), INTENT(IN) :: message
+  CALL write_to_log(log_level_fatal, proc_name, message)
 END SUBROUTINE log_fatal
-
 END MODULE logging_mod
 #endif
