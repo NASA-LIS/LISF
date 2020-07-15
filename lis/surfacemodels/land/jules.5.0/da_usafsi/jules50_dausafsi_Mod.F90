129389282515cd3f601950e615bb3d0b793ca8df
James V. Geiger <james.v.geiger@nasa.gov>
2020-07-15 14:21:58 -0400

    Import LIS/JULES.5.0 interface code from restricted-lisf-jules
    This is commit 1244564b169b5b6d022cecb3a1c537dba6cf88e4 from
    NASA-LIS/restricted-lisf-jules.

---
+++ lis/surfacemodels/land/jules.5.0/da_usafsi/jules50_dausafsi_Mod.F90
@@ -0,0 +1,50 @@
+!-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+! NASA Goddard Space Flight Center Land Information System (LIS) v7.2
+!
+! Copyright (c) 2015 United States Government as represented by the
+! Administrator of the National Aeronautics and Space Administration.
+! All Rights Reserved.
+!-------------------------END NOTICE -- DO NOT EDIT-----------------------
+#include "LIS_misc.h"
+module jules50_dausafsi_Mod
+!BOP
+!
+! !MODULE: jules50_dausafsi_Mod
+!
+! !DESCRIPTION:
+!  
+! !REVISION HISTORY:
+! 08 Jul 2019: Yeosang Yoon; Modified for Jules.5.0 and LDT-SI data
+! 13 Dec 2019: Eric Kemp; Replaced LDTSI with USAFSI
+!
+! !USES:        
+
+  implicit none
+
+  PRIVATE
+!-----------------------------------------------------------------------------
+! !PUBLIC MEMBER FUNCTIONS:
+!-----------------------------------------------------------------------------
+  public :: jules50_dausafsi_init
+!-----------------------------------------------------------------------------
+! !PUBLIC TYPES:
+!-----------------------------------------------------------------------------
+!EOP
+
+  SAVE
+contains
+!BOP
+! 
+! !ROUTINE: jules50_dausafsi_init
+! \label{jules50_dausafsi_init}
+! 
+! !INTERFACE:
+  subroutine jules50_dausafsi_init()
+! !USES:
+! !DESCRIPTION:        
+!
+!EOP
+    implicit none
+
+  end subroutine jules50_dausafsi_init
+end module jules50_dausafsi_Mod
