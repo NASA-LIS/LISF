f2ca865b7b6f0c9be0e39679148d395950251bc0
Brendan McAndrew <brendan.b.mcandrew@nasa.gov>
2020-09-01 10:08:02 -0400

    Fix SMAP readers to allow compile w/o HDF5
    HDF5 is an optional library, but LVT would fail to compile without it.
    
    Changes:
    In datastreams/SMAPTB/readSMAPTBobs.F90:
       * For-loop at end of `read_SMAPTB` subroutine moved inside `if (defined USE_HDF5)` block
    
    In datastreams/SMAP_L3TB/readSMAP_L3TB.F90 and datastreams/SMAPvod/readSMAPvodobs.F90:
       * Variable declarations for `source` and `filename` moved outside `if (defined USE_HDF5)` which is consistent with readSMAPTBobs.F90

--- lvt/datastreams/SMAPvod/readSMAPvodobs.F90
+++ lvt/datastreams/SMAPvod/readSMAPvodobs.F90
@@ -379,6 +379,8 @@ subroutine read_SMAPvod(source, fname, vodobs)
 
   implicit none
 
+  integer                        :: source 
+  character(len=*)               :: fname
   real                           :: vodobs(LVT_rc%lnc,LVT_rc%lnr)
   
 !
@@ -397,8 +399,7 @@ subroutine read_SMAPvod(source, fname, vodobs)
 
 #if (defined USE_HDF5)
 
-  integer                       :: source 
-  character(len=*)              :: fname
+ 
   character*100,   parameter    :: vod_gr_name = "Soil_Moisture_Retrieval_Data"
   character*100,   parameter    :: vod_field_name = "vegetation_opacity"
 
