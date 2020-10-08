8aada4945d7454bcb068190777570d3558e83a2a
Timothy M. Lahmers <tlahmers@discover16.prv.cube>
2020-10-08 17:35:05 -0400

    Add getswepred module to LIS rebase; needed for snow DA.

---
+++ lis/surfacemodels/land/noahmp.4.0.1/da_snow/noahmp401_getswepred.F90
@@ -0,0 +1,46 @@
+!-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+! NASA Goddard Space Flight Center Land Information System (LIS) v7.2
+!
+! Copyright (c) 2015 United States Government as represented by the
+! Administrator of the National Aeronautics and Space Administration.
+! All Rights Reserved.
+!-------------------------END NOTICE -- DO NOT EDIT-----------------------
+!BOP
+! !ROUTINE: noahmp401_getswepred
+! \label{noahmp401_getswepred}
+!
+! !REVISION HISTORY:
+! 27Feb2005: Sujay Kumar; Initial Specification
+! 25Jun2006: Sujay Kumar: Updated for the ESMF design
+!  02 Mar 2010: Sujay Kumar; Modified for Noah 3.1
+!
+! !INTERFACE:
+subroutine noahmp401_getswepred(n, k, obs_pred)
+
+! !USES:
+  use ESMF
+  use LIS_coreMod, only : LIS_rc,LIS_surface
+  use noahmp401_lsmMod
+  use LIS_DAobservationsMod
+
+  implicit none
+! !ARGUMENTS: 
+  integer, intent(in)    :: n
+  integer, intent(in)    :: k
+  real                   :: obs_pred(LIS_rc%ngrid(n),LIS_rc%nensem(n))
+  real                   :: swe(LIS_rc%npatch(n,LIS_rc%lsm_index))
+!EOP
+
+  integer                :: t
+
+  do t=1,LIS_rc%npatch(n,LIS_rc%lsm_index)
+     swe(t) = noahmp401_struc(n)%noahmp401(t)%sneqv !obs in mm
+  enddo
+
+  call LIS_convertPatchSpaceToObsEnsSpace(n,k,&
+       LIS_rc%lsm_index, &
+       swe,&
+       obs_pred)
+
+end subroutine noahmp401_getswepred
+
