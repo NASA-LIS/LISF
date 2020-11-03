89e804116452162c8edd7338039998e9e1ec5cb4
Eric Kemp <eric.kemp@nasa.gov>
2020-11-03 17:03:03 -0500

    Added utility to update lis.config.
    TODO:  Need to update autotune.py to not read command line arguments.  This
    should instead be performed by the calling scripts, with arguments passed
    to the AutomateTuning class as needed.
    
    This change will remove the need to specify data source, end date/time, and
    day range for customize_lis_config.py

---
+++ lis/utils/afwa/retune_bratseth/scripts/customize_lis_config.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+SCRIPT: customize_lis_config.py
+
+Customizes lis.config file with latest Bratseth error settings.
+
+REVISION HISTORY:
+03 Nov 2020:  Eric Kemp.  Initial specification.
+"""
+
+import autotune
+
+if __name__ == "__main__":
+
+    # Construct automator object, which reads the command line and config
+    # file.
+    AUTOMATOR = autotune.AutomateTuning()
+
+    # Get the covariance settings
+    for var in ["rh2m", "spd10m", "t2m", \
+                "gage",
+                "cmorph", "geoprecip", "imerg", "ssmi"]:
+        AUTOMATOR.get_bratseth_err_settings(var)
+
+    # Assemble new lines for lis.config file
+    AUTOMATOR.assemble_new_lines()
+
+    # Customize the lis.config file.
+    AUTOMATOR.customize_lis_config()
+
+
