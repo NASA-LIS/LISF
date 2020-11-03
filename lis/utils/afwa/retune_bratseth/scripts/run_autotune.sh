130d8d18179482d3d57489f7b9f21b6f77fb35e9
Eric Kemp <eric.kemp@nasa.gov>
2020-11-03 13:37:29 -0500

    Added support for other gage-to-satellite comparisons.

--- lis/utils/afwa/retune_bratseth/scripts/run_autotune.sh
+++ lis/utils/afwa/retune_bratseth/scripts/run_autotune.sh
@@ -41,14 +41,43 @@ enddt=$1
 dayrange=$2
 
 # First, handle non-precipitation
-for varname in rh2m spd10m t2m ; do
-    $SCRIPTDIR/customize_procoba_nwp.py $CFGDIR/autotune.cfg \
-                                        $varname $enddt $dayrange || exit 1
-    mpirun -np $SLURM_NTASKS $BINDIR/procOBA_NWP \
-           procOBA_NWP.$varname.config || exit 1
-    $SCRIPTDIR/fit_semivariogram.py $CFGDIR/$varname.cfg \
-                                    $varname.param || exit 1
-done
+# for varname in rh2m spd10m t2m ; do
+#
+#     $SCRIPTDIR/customize_procoba_nwp.py $CFGDIR/autotune.cfg \
+#                                         $varname $enddt $dayrange || exit 1
+#
+#     mpirun -np $SLURM_NTASKS $BINDIR/procOBA_NWP \
+#            procOBA_NWP.$varname.config || exit 1
+#
+#     $SCRIPTDIR/fit_semivariogram.py $CFGDIR/$varname.cfg \
+#                                     $varname.param || exit 1
+#
+# done
 
+# # Next, handle gages versus NWP
+#
+# $SCRIPTDIR/customize_procoba_nwp.py $CFGDIR/autotune.cfg \
+#                                     gage $enddt $dayrange || exit 1
+#
+# mpirun -np $SLURM_NTASKS $BINDIR/procOBA_NWP \
+#        procOBA_NWP.gage.config || exit 1
+#
+# $SCRIPTDIR/fit_semivariogram.py $CFGDIR/gage_nwp.cfg \
+#                                 gage_nwp.param || exit 1
+
+
+# Next, handle gages versus satellite data
+for varname in imerg cmorph geoprecip ssmi ; do
+
+    $SCRIPTDIR/customize_procoba_sat.py $CFGDIR/autotune.cfg \
+                                        $varname $enddt $dayrange || exit 1
+
+    mpirun -np $SLURM_NTASKS $BINDIR/procOBA_Sat \
+           procOBA_Sat.$varname.config || exit 1
+
+    $SCRIPTDIR/fit_semivariogram.py $CFGDIR/gage_$varname.cfg \
+                                    gage_$varname.param || exit 1
+
+done
 
 exit 0
