a7537dbe7776ab61dc707cd89d60f511aff19daa
Eric Kemp <eric.kemp@nasa.gov>
2020-11-20 14:17:07 -0500

    Corrected first line of scripts to use python3.

--- lis/utils/afwa/retune_bratseth/scripts/customize_lis_config.py
+++ lis/utils/afwa/retune_bratseth/scripts/customize_lis_config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 SCRIPT: customize_lis_config.py
 
--- lis/utils/afwa/retune_bratseth/scripts/rescale_sat_sigma2.py
+++ lis/utils/afwa/retune_bratseth/scripts/rescale_sat_sigma2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 SCRIPT: rescale_sat_sigma2.py
 
