250c27528ce83349ca057483466034c8bc078b20
Eric Kemp <eric.kemp@nasa.gov>
2020-12-15 15:43:17 -0500

    Updated batch script to run more efficiently.
    Includes new names for log files.

--- lis/utils/afwa/retune_bratseth/scripts/run_autotune.sh
+++ lis/utils/afwa/retune_bratseth/scripts/run_autotune.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 #SBATCH --job-name=autotune
 #SBATCH --time=1:00:00
-##SBATCH --time=3:00:00
 #SBATCH --account s1189
 #SBATCH --output autotune.slurm.out
 #Adjust node, core, and hardware constraints here
-#SBATCH --ntasks=1 --constraint=hasw
+#SBATCH --ntasks=4 --mem-per-cpu=4G --constraint=hasw
 #Substitute your e-mail here
 #SBATCH --mail-user=eric.kemp@nasa.gov
 #SBATCH --mail-type=ALL
 #Set quality of service, if needed.
-#SBATCH --qos=debug
+##SBATCH --qos=debug
 
 ulimit -s unlimited
 
@@ -41,50 +40,92 @@ fi
 enddt=$1
 dayrange=$2
 
-# First, handle NWP semivariographic analyses
+# Customize config files for procOBA_NWP, including blacklist creation
 for varname in gage rh2m spd10m t2m ; do
-
+    echo `date`
     echo "Calling customize_procoba_nwp.py for $varname"
-    $SCRIPTDIR/customize_procoba_nwp.py $CFGDIR/autotune.cfg \
-        $enddt $dayrange $varname || exit 1
+    srun --ntasks=1 \
+         $SCRIPTDIR/customize_procoba_nwp.py $CFGDIR/autotune.cfg \
+         $enddt $dayrange $varname &
+    sleep 1
+done
+wait
 
+# Next, construct empirical semivariograms
+for varname in gage rh2m spd10m t2m ; do
+    echo `date`
     echo "Calling procOBA_NWP for $varname"
-    mpirun -np $SLURM_NTASKS $BINDIR/procOBA_NWP \
-        procOBA_NWP.$varname.config || exit 1
+    srun --ntasks=1  \
+         $BINDIR/procOBA_NWP procOBA_NWP.$varname.config \
+         procOBA_NWP.$varname.log &
+    sleep 1
+done
+wait
 
+# Now fit the semivariogram functions
+for varname in gage rh2m spd10m t2m ; do
+    echo `date`
     echo "Calling fit_semivariogram.py for $varname"
     if [ $varname = gage ] ; then
-        $SCRIPTDIR/fit_semivariogram.py $CFGDIR/${varname}_nwp.cfg \
-            ${varname}_nwp.param || exit 1
+        srun --ntasks=1 \
+             $SCRIPTDIR/fit_semivariogram.py $CFGDIR/${varname}_nwp.cfg \
+            ${varname}_nwp.param &
     else
-        $SCRIPTDIR/fit_semivariogram.py $CFGDIR/$varname.cfg \
-            $varname.param || exit 1
+        srun --ntasks=1 \
+             $SCRIPTDIR/fit_semivariogram.py $CFGDIR/$varname.cfg \
+            $varname.param &
     fi
-
+    sleep 1
 done
+wait
 
-
-# Next, handle satellite variographic analyses
+# Next, customize config files for procOBA_Sat
 for varname in cmorph geoprecip imerg ssmi ; do
-
+    echo `date`
     echo "Calling customize_procoba_sat.py for $varname"
-    $SCRIPTDIR/customize_procoba_sat.py $CFGDIR/autotune.cfg \
-        $enddt $dayrange $varname || exit 1
-
-    echo "Calling procOBA_Sat for $varname"
-    mpirun -np $SLURM_NTASKS $BINDIR/procOBA_Sat \
-        procOBA_Sat.$varname.config || exit 1
-
-    echo "Calling fit_semivariogram.py for $varname"
-    $SCRIPTDIR/fit_semivariogram.py $CFGDIR/gage_$varname.cfg \
-        gage_$varname.param || exit 1
-
-    # Rescale the satellite error variance to compare with NWP.
-    echo "Calling rescale_sat_sigma2.py for $varname"
-    $SCRIPTDIR/rescale_sat_sigma2.py $varname || exit 1
+    srun --ntasks=1 \
+         $SCRIPTDIR/customize_procoba_sat.py $CFGDIR/autotune.cfg \
+         $enddt $dayrange $varname &
+    sleep 1
 done
+wait
+
+# Next, construct empirical semivariograms
+for varname in cmorph geoprecip imerg ssmi ; do
+    echo `date`
+    echo "Calling procOBA_Sat for $varname"
+    #mpirun -np $SLURM_NTASKS $BINDIR/procOBA_Sat \
+    #    procOBA_Sat.$varname.config || exit 1
+    srun --ntasks=1 \
+         $BINDIR/procOBA_Sat procOBA_Sat.$varname.config \
+         procOBA_Sat.$varname.log &
+    sleep 1
+done
+wait
+
+# Next, fit semivariograms for satellite data
+for varname in cmorph geoprecip imerg ssmi ; do
+    echo `date`
+    echo "Calling fit_semivariogram.py for $varname"
+    srun --ntasks=1 \
+         $SCRIPTDIR/fit_semivariogram.py $CFGDIR/gage_$varname.cfg \
+         gage_$varname.param &
+    sleep 1
+done
+wait
+
+# Next, rescale the satellite error variance to compare with NWP.
+for varname in cmorph geoprecip imerg ssmi ; do
+    echo `date`
+    echo "Calling rescale_sat_sigma2.py for $varname"
+    srun --ntasks=1 \
+         $SCRIPTDIR/rescale_sat_sigma2.py $varname &
+    sleep 1
+done
+wait
 
 # Update the lis.config error settings
+echo `date`
 echo "Calling customize_lis_config.py"
 $SCRIPTDIR/customize_lis_config.py $CFGDIR/autotune.cfg \
     $enddt $dayrange || exit 1
