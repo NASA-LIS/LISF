2e35b734fc703cf3e4db2a9acd4f0318c5337991
dpsarmie <42810219+dpsarmie@users.noreply.github.com>
2021-01-12 10:42:07 -0500

    Update noahmp401_getsws_hymap2.F90

--- lis/surfacemodels/land/noahmp.4.0.1/routing/noahmp401_getsws_hymap2.F90
+++ lis/surfacemodels/land/noahmp.4.0.1/routing/noahmp401_getsws_hymap2.F90
@@ -42,7 +42,6 @@ subroutine noahmp401_getsws_hymap2(n)
   integer                :: status
   integer                :: enable2waycpl
   
-  enable2waycpl = 0 
   if(enable2waycpl==1) then 
 
      write(LIS_logunit,*) '[ERR] Two-way coupling between NoahMP401 and HYMAP2'
