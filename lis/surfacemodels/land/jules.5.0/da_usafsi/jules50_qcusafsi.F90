9466e23991aab5572f09ac5a321b8f059383d1c3
David Mocko <David.Mocko@nasa.gov>
2021-01-19 10:15:51 -0500

    Refine JULES-5.0 snow DA checks against attrib file maximums
    This commit refines JULES-5.0 snow DA checks as recommended
    by @yyoon, @yhkwon81, and @emkemp on the comments to this
    pull request.
    
    Instead of hard-coding a value above which snow DA is not
    performed, the maximum values in the attribs file are used.
    
    Also, consistency checks between the SWE and snow depth
    after snow DA against the maximums are performed, using
    the snow density.
    
    The recommended JULES-5.0 SWE and snow depth maximums are
    lowered to their previous values, and now are consistent
    with the numbers used by Noah-3.9 and Noah-MP-4.0.1 LSMs.
    
    (cherry picked from commit b2f7a10aeb5dc6568614fa680eb283eaa4e32249)

--- lis/surfacemodels/land/jules.5.0/da_usafsi/jules50_qcusafsi.F90
+++ lis/surfacemodels/land/jules.5.0/da_usafsi/jules50_qcusafsi.F90
@@ -25,6 +25,12 @@
 !              Only check SWE/snow maximum when doing update;
 !              Turn off DA in high snow depth regions;
 !                  (from Eric Kemp's SNODEP qc change)
+! 19 Jan 2020: David Mocko; Further tweaks to checking high snow
+!              depth to turn off DA (now using attributes file);
+!              Added consistency checks to setting maximum SWE
+!              and snow depth after DA against the snow density;
+!              Changes recommended after discussion with:
+!                   Yeosang Yoon, Yonghwan Kwon, Eric Kemp
 !
 ! !INTERFACE:
 subroutine jules50_qcusafsi(n, LSM_State)
@@ -93,12 +99,11 @@ subroutine jules50_qcusafsi(n, LSM_State)
           LIS_surface(n,LIS_rc%lsm_index)%tile(t)%col,&
           LIS_surface(n,LIS_rc%lsm_index)%tile(t)%row)
 
-     if ((snod(t).lt.snodmin) .or. swe(t).lt.swemin) then
+     if ((snod(t).lt.snodmin).or.(swe(t).lt.swemin)) then
         update_flag(gid) = .false.
      endif
 
-!A more strict "deep snow" check, independent of the ancillary data. - EMK
-     if (snod(t).gt.10.0) then
+     if ((snod(t).gt.snodmax).or.(swe(t).gt.swemax)) then
         update_flag(gid) = .false.
      endif
 
@@ -129,11 +134,22 @@ subroutine jules50_qcusafsi(n, LSM_State)
 
         if (swe(t).gt.swemax) then
            swe(t) = swemax
+           snod(t) = swe(t)/sndens
         endif
         if (snod(t).gt.snodmax) then
            snod(t) = snodmax
+           swe(t)  = snod(t)*sndens
         endif
 
+        if (swe(t).lt.swemin) then
+           swe(t) = swemin
+           snod(t) = swe(t)/sndens
+        endif
+        if (snod(t).lt.snodmin) then
+           snod(t) = snodmin
+           swe(t)  = snod(t)*sndens
+        endif
+        
 !If the update is unphysical, do not update
      else
         snod(t) = jules50_struc(n)%jules50(t)%snowdepth(pft)
