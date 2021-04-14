a58833a6274176331846479a1148965cd8242c2c
Melissa Wrzesien <melissa.l.wrzesien@nasa.gov>
2021-04-14 10:44:59 -0400

    Add support for SNODAS DA

--- lis/plugins/LIS_lsmda_pluginMod.F90
+++ lis/plugins/LIS_lsmda_pluginMod.F90
@@ -479,7 +479,6 @@ subroutine LIS_lsmda_plugin
    external NoahMP401_getsnowvars         
    external NoahMP401_setsnowvars              
    external NoahMP401_getsnowpred
-!   external NoahMP401_getsnwdpred  !MLW 
    external NoahMP401_getswepred
    external NoahMP401_qcsnow
    external NoahMP401_qc_snowobs
@@ -2572,30 +2571,26 @@ subroutine LIS_lsmda_plugin
    call registerlsmdadescalestatevar(trim(LIS_noahmp401Id)//"+"//&
         trim(LIS_MCD15A2HlaiobsId)//char(0),noahmp401_descale_veg)
 
-! Melissa Wrzesien,
-! NoahMP 4.0.1  GCOMW AMSR2 snow depth
    call registerlsmdainit(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),noahmp401_dasnow_init)
+        trim(LIS_SNODASobsId)//char(0),noahmp401_dasnow_init)
    call registerlsmdagetstatevar(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_getsnowvars)
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_getsnowvars)
    call registerlsmdasetstatevar(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_setsnowvars)
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_setsnowvars)
    call registerlsmdagetobspred(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_getsnowpred)
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_getsnowpred)
    call registerlsmdaqcstate(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_qcsnow)
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_qcsnow)
    call registerlsmdaqcobsstate(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_qc_snowobs)
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_qc_snowobs)
    call registerlsmdascalestatevar(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_scale_snow)
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_scale_snow)
    call registerlsmdadescalestatevar(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_descale_snow)
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_descale_snow)
    call registerlsmdaupdatestate(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_updatesnowvars)
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_updatesnowvars)
    call registerlsmdaqcobsstate(trim(LIS_noahmp401Id)//"+"//&
-        trim(LIS_GCOMW_AMSR2L3sndobsId)//char(0),NoahMP401_qc_snowobs)
-! MW
-
+        trim(LIS_SNODASobsId)//char(0),NoahMP401_qc_snowobs)
 
 ! Yeosang Yoon, SNODEP DA
 #if ( defined DA_OBS_SNODEP )
