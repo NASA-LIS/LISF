c4592b7a2f82a1aef39e38361762651b47c96a30
Eric Kemp <eric.kemp@nasa.gov>
2021-05-05 15:48:48 -0400

    Replaced print* and stop.

--- lis/surfacemodels/land/jules.5.2/da_soilm/jules52_qc_soilmobs.F90
+++ lis/surfacemodels/land/jules.5.2/da_soilm/jules52_qc_soilmobs.F90
@@ -20,7 +20,7 @@ subroutine jules52_qc_soilmobs(n,k,OBS_State)
 ! !USES:
   use ESMF
   use LIS_coreMod
-  use LIS_logMod,  only : LIS_verify
+  use LIS_logMod,  only : LIS_verify, LIS_logunit, LIS_endrun
   use LIS_constantsMod, only : LIS_CONST_TKFRZ
   use LIS_DAobservationsMod
   use jules52_lsmMod
@@ -199,8 +199,8 @@ sneqv = 0
       !MN:fveg is used for 12-month green vegetation fraction (i.e., noah33_struc(n)%noah(:)%shdfac)  
       !print*,'l_aggregate', l_aggregate
       if(.NOT. l_aggregate) then   
-         print*,'Please set the l_aggregate to .true. in the jules_surface.nml ' 
-         stop         
+         write(LIS_logunit,*) 'Please set the l_aggregate to .true. in the jules_surface.nml ' 
+         call LIS_endrun
       else
        do l=1,5 !Broadleaf trees, Needleleaf trees, C3 (temperate) grass, C4 (tropical) grass, Shrubs
          fveg(t) = fveg(t) + jules52_struc(n)%jules52(t)%surft_frac(l)   
