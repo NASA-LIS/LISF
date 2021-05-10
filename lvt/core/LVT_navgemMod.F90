c87d4a5efc74dbc53fc204b72f0c49d8663e6ae5
Eric Kemp <eric.kemp@nasa.gov>
2021-05-10 18:28:14 -0400

    Added temporary support for Navy "degribbed" binary SST files.

--- lvt/core/LVT_navgemMod.F90
+++ lvt/core/LVT_navgemMod.F90
@@ -31,6 +31,8 @@ module LVT_navgemMod
   ! Public routines
   public :: LVT_get_navgem_sst_gr1_filename
   public :: LVT_fetch_navgem_sst_gr1_field
+  public :: LVT_get_navgem_sst_bin_filename
+  public :: LVT_fetch_navgem_sst_bin_field
 
 contains
 
@@ -248,7 +250,7 @@ contains
        do i = 1, Ni
           ilon = ilon + iDirectionIncrement
           if (ilon .ge. 180000 .and. &
-              (ilon - iDirectionIncrement) .lt. 180000) then
+               (ilon - iDirectionIncrement) .lt. 180000) then
              i_180 = i
              iNewStartLon = ilon - 360000
              iNewEndLon = ilon - iDirectionIncrement
@@ -340,4 +342,232 @@ contains
     call grib_close_file(ftn)
 
   end subroutine LVT_fetch_navgem_sst_gr1_field
+
+  subroutine construct_navgem_sst_bin_filename(rootdir, &
+       year, month, day, hour, filename)
+
+    ! Defaults
+    implicit none
+
+    ! Arguments
+    character(len=*), intent(in)  :: rootdir
+    integer, intent(in) :: year
+    integer, intent(in) :: month
+    integer, intent(in) :: day
+    integer, intent(in) :: hour
+    character(len=*), intent(out) :: filename
+
+    ! Local variables
+    character(len=10) :: yyyymmddhh
+
+    write(yyyymmddhh,'(i4.4,i2.2,i2.2,i2.2)') year, month, day, hour
+
+    ! FIXME:  Update file name to match that provided by 557WW.  The
+    ! existing code is for a sample file provided by FNMOC.
+    filename = trim(rootdir) // '/navyssts_0p25deg.' &
+         // yyyymmddhh &
+         // '.dat'
+
+  end subroutine construct_navgem_sst_bin_filename
+
+  ! Select binary file with Navy 0.25deg SST data.  Based on SNODEP/USAFSI
+  ! code.
+  subroutine LVT_get_navgem_sst_bin_filename(filename, &
+       year, month, day, hour)
+
+    ! Modules
+    use LVT_logMod, only: LVT_logunit
+    use LVT_timeMgrMod, only: LVT_get_julhr, LVT_julhr_date
+
+    ! Defaults
+    implicit none
+
+    ! Arguments
+    character(len=*), intent(inout) :: filename
+    integer, intent(out) :: year
+    integer, intent(out) :: month
+    integer, intent(out) :: day
+    integer, intent(out) :: hour
+
+    ! Locals
+    integer :: navgem_julhr, lvt_julhr
+    logical :: file_exists
+
+    ! FIXME...Add dynamic search for nearest NAVGEM file.  The
+    ! existing code is hardwired for a sample file.
+    year = 2021
+    month = 05
+    day = 05
+    hour = 12
+
+    call construct_navgem_sst_bin_filename('./navgem', &
+         year, month, day, hour, filename)
+
+    write(LVT_logunit,*)'[INFO] *** Searching for NAVGEM file ', &
+         trim(filename)
+    inquire(file=trim(filename), exist=file_exists)
+    if (file_exists) then
+       write(LVT_logunit,*)'[INFO] Will use ', trim(filename)
+       return
+    end if
+
+    ! FIXME...Add dynamic search for NAVGEM file
+    write(LVT_logunit,*)'[ERR] Cannot find NAVGEM file!'
+    stop
+
+  end subroutine LVT_get_navgem_sst_bin_filename
+
+  ! Binary file reader. Taken from SNODEP/USAFSI
+  subroutine putget_real(buffer, iofunc, file_name, igrid, jgrid)
+
+    ! Modules
+    use LVT_logMod, only: LVT_logunit
+
+    ! Defaults
+    implicit none
+
+    ! Arguments
+    real, intent(inout) :: buffer(igrid, jgrid)
+    character*1, intent(in) :: iofunc
+    character(len=*), intent(in) :: file_name
+    integer, intent(in) :: igrid
+    integer, intent(in) :: jgrid
+
+    ! Locals
+    integer, parameter :: msglns = 20
+    character*14 :: access_type
+    character*4 :: cstat
+    character*90 :: message(msglns)
+    integer :: istat
+    integer :: istat1
+    integer :: reclen
+    logical :: isopen
+    integer :: i
+
+    isopen  = .false.
+    message = ' '
+    reclen  = igrid * jgrid * 4
+
+    access_type = '[INFO] Opening'
+    open( 2, file=trim(file_name), form='unformatted', convert='little_endian', &
+         access='direct', recl=reclen, iostat=istat, err=1000 )
+    isopen = .true.
+
+    ! Read from file, abort on error
+    if ( (iofunc .eq. 'r') .or. (iofunc .eq. 'R') ) then
+       access_type = '[INFO] Reading'
+       read( 2, rec=1, iostat=istat, err=1000 ) buffer
+
+       ! Write to file, abort on error
+    else if ( (iofunc .eq. 'w') .or. (iofunc .eq. 'W') ) then
+       access_type = '[INFO] Writing'
+       write( 2, rec=1, iostat=istat, err=1000 ) buffer
+
+       ! Else abort due to invalid IOFUNC value.
+    else
+       go to 4000
+    endif
+
+    close(2)
+
+    return
+
+    ! Error handling
+1000 continue
+    if (isopen) close (2)
+    message(1) = '[ERR] LIBROUTINE: PUTGET_REAL'
+    message(2) = '[ERR] ERROR ' // access_type // ' ' // trim (file_name)
+    write (cstat, '(i4)', iostat=istat1) istat
+    if (istat1 .eq. 0) message(3) = 'ISTAT = ' // cstat
+    do i = 1, 3
+       write(LVT_logunit,*) trim(message(i))
+    end do
+    return
+
+4000 continue
+    close(2)
+    message(1) = '[ERR] LIBROUTINE: PUTGET_REAL'
+    message(2) = '[ERR] INVALID IOFUNC VALUE = '// trim(iofunc)
+    do i = 1, 2
+       write(LVT_logunit,*) trim(message(i))
+    end do
+    return
+
+  end subroutine putget_real
+
+  ! Routine for fetching merged sea surface temperature/land surface
+  ! field.  We refer to this as "SST" for simplicity.
+  subroutine LVT_fetch_navgem_sst_bin_field(filename, sst, gridDesc)
+
+    ! Modules
+    use LVT_coreMod, only: LVT_rc
+    use LVT_logMod, only: LVT_logunit, LVT_verify
+
+    ! Defaults
+    implicit none
+
+    ! Arguments
+    character(len=*), intent(in) :: filename
+    real, allocatable, intent(out) :: sst(:)
+    real, intent(out) :: gridDesc(50)
+
+    ! Locals
+    integer, parameter :: sst_igrid = 1440
+    integer, parameter :: sst_jgrid = 721
+    integer, parameter :: sst_size = sst_igrid * sst_jgrid
+    integer :: year, month, day, hour, fcst_hr
+    logical :: file_exists
+    real, allocatable :: sst2d(:,:)
+    integer :: i, j, i_180, i_rotate
+
+    inquire(file=trim(filename), exist=file_exists)
+    if (.not. file_exists) return
+    write(LVT_logunit,*)'[INFO] Processing ', trim(filename)
+
+    allocate(sst2d(sst_igrid, sst_jgrid))
+    sst2d = LVT_rc%udef
+    call putget_real(sst2d, 'r', trim(filename), sst_igrid, sst_jgrid)
+
+    ! This grid ranges from 0 to 360E, but we need to rotate to -180 to 180
+    ! E.
+    allocate(sst(sst_size))
+    sst = -1
+    i_180 = 720
+    do j = 1, sst_jgrid
+       do i = 1, sst_igrid
+          i_rotate = i + i_180 - 1
+          if (i_rotate .gt. sst_igrid) then
+             i_rotate = i_rotate - sst_igrid
+          end if
+          sst(i_rotate + (j-1)*sst_igrid) = sst2d(i,j)
+       end do
+    end do
+    deallocate(sst2d)
+
+    griddesc = 0
+    griddesc(1) = 0 ! Lat/lon projection
+    griddesc(2) = sst_igrid
+    griddesc(3) = sst_jgrid
+    griddesc(4) = -90.
+    griddesc(5) = -180.! Rotated grid
+    gridDesc(6) = 128
+    gridDesc(7) = 90.
+    gridDesc(8) = 179.75 ! Rotated grid
+    gridDesc(9) = 0.25
+    gridDesc(10) = 0.25
+    gridDesc(11) = 64
+    gridDesc(20) = 64  ! E-W ordering
+    gridDesc(30) = 0 ! Lat/lon projection
+    griddesc(32) = sst_igrid
+    griddesc(33) = sst_jgrid
+    griddesc(34) = -90.
+    griddesc(35) = -180. ! Rotated grid
+    gridDesc(36) = 128
+    gridDesc(37) = 90.
+    gridDesc(38) = 179.75 ! Rotated grid
+    gridDesc(39) = 0.25
+    gridDesc(40) = 0.25
+
+  end subroutine LVT_fetch_navgem_sst_bin_field
+
 end module LVT_navgemMod
