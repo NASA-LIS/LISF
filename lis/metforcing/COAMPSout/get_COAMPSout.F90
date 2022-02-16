48f60d391f71de5b6f0f16cc924c40b9293f4b3e
Brendan McAndrew <brendan.b.mcandrew@nasa.gov>
2022-02-16 17:02:24 -0500

    Update copyright notice

--- lis/metforcing/COAMPSout/get_COAMPSout.F90
+++ lis/metforcing/COAMPSout/get_COAMPSout.F90
@@ -1,7 +1,9 @@
 !-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
-! NASA Goddard Space Flight Center Land Information System (LIS) v7.2
+! NASA Goddard Space Flight Center
+! Land Information System Framework (LISF)
+! Version 7.3
 !
-! Copyright (c) 2015 United States Government as represented by the
+! Copyright (c) 2020 United States Government as represented by the
 ! Administrator of the National Aeronautics and Space Administration.
 ! All Rights Reserved.
 !-------------------------END NOTICE -- DO NOT EDIT-----------------------
@@ -255,4 +257,3 @@ end subroutine get_COAMPSout
         trim(fda)//trim(fhr1)//'_00'//trim(fhr)//'0000.nc'
       
  end subroutine COAMPSoutfile
-
