dfe051caa274c4f7949322caea0910359e02a022
Sujay V. Kumar <Sujay.V.Kumar@nasa.gov>
2022-03-17 14:28:06 -0400

    Updates the H2 offline capability
    Allows H2 to run with LIS outputs that are not necessarily colocated with the LIS domain.
    The outputs can be from a larger domain with different spatial resolution
    
    Resolves #1017

--- lis/routing/HYMAP2_router/runoffdata/LISoutput/LISrunoffdataMod.F90
+++ lis/routing/HYMAP2_router/runoffdata/LISoutput/LISrunoffdataMod.F90
@@ -37,11 +37,16 @@ module LISrunoffdataMod
   
   type, public :: LISrunoffdatadec
      
-     real                :: outInterval 
-     character*50        :: odir
- 
+     real                 :: outInterval 
+     character*100        :: odir
+     character*100        :: domfile
+
+     integer, allocatable :: n11(:)
      !ag - 17Mar2016
+     logical             :: domaincheck
+     integer             :: nc,nr
      character*100       :: previous_filename
+     real                :: datares
      real, allocatable   :: qs(:,:),qsb(:,:),evap(:,:)
      
   end type LISrunoffdatadec
@@ -60,10 +65,21 @@ contains
     use LIS_coreMod
     use LIS_logMod
     use LIS_timeMgrMod
-
+#if (defined USE_NETCDF3 || defined USE_NETCDF4)
+    use netcdf
+#endif
+    
     integer              :: n 
     integer              :: status
+    integer              :: ftn
+    character*100        :: lis_map_proj
     character*10         :: time
+    integer              :: nc,nr
+    integer              :: ncId, nrId
+    real                 :: lat1,lat2
+    real                 :: lon1,lon2
+    real                 :: dx,dy
+    real                 :: gridDesc(50)
 
     allocate(LISrunoffdata_struc(LIS_rc%nnest))
        
@@ -85,6 +101,108 @@ contains
 
        call LIS_parseTimeString(time,LISrunoffdata_struc(n)%outInterval)
     enddo
+
+    call ESMF_ConfigFindLabel(LIS_config,&
+         "LIS runoff output domain file:",rc=status)
+    do n=1, LIS_rc%nnest
+       call ESMF_ConfigGetAttribute(LIS_config,LISrunoffdata_struc(n)%domfile,rc=status)
+       call LIS_verify(status,&
+            "LIS runoff output domain file: not defined")
+       
+    enddo
+
+#if (defined USE_NETCDF3 || defined USE_NETCDF4)
+
+    do n=1,LIS_rc%nnest
+       call LIS_verify(nf90_open(path=LISrunoffdata_struc(n)%domfile,&
+            mode=NF90_NOWRITE,ncid=ftn),&
+            'Error opening file '//trim(LISrunoffdata_struc(n)%domfile))
+       
+       call LIS_verify(nf90_get_att(ftn,NF90_GLOBAL,'MAP_PROJECTION',&
+            lis_map_proj),&
+            'Error in nf90_get_att: MAP_PROJECTION')
+       
+       if(lis_map_proj.eq."EQUIDISTANT CYLINDRICAL") then
+          
+          call LIS_verify(nf90_inq_dimid(ftn,'east_west',ncId),&
+               'Error in nf90_inq_dimid: east_west')
+          
+          call LIS_verify(nf90_inq_dimid(ftn,'north_south',nrId),&
+               'Error in nf90_inq_dimid: north_south')
+          
+          call LIS_verify(nf90_inquire_dimension(ftn,ncId,len=LISrunoffdata_struc(n)%nc),&
+               'Error in nf90_inquire_dimension: ncId')
+          
+          call LIS_verify(nf90_inquire_dimension(ftn,nrId,len=LISrunoffdata_struc(n)%nr),&
+               'Error in nf90_inquire_dimension: nrId')
+
+          LISrunoffdata_struc(n)%domainCheck = .true.
+          
+          if(LISrunoffdata_struc(n)%nc.ne.LIS_rc%lnc(n).or.&
+               LISrunoffdata_struc(n)%nr.ne.LIS_rc%lnr(n)) then
+             
+             LISrunoffdata_struc(n)%domainCheck = .false. 
+             call LIS_verify(nf90_get_att(ftn,NF90_GLOBAL,'SOUTH_WEST_CORNER_LAT',&
+                  lat1),&
+                  'Error in nf90_get_att: SOUTH_WEST_CORNER_LAT')
+             
+             call LIS_verify(nf90_get_att(ftn,NF90_GLOBAL,'SOUTH_WEST_CORNER_LON',&
+                  lon1),&
+                  'Error in nf90_get_att: SOUTH_WEST_CORNER_LON')
+             
+             call LIS_verify(nf90_get_att(ftn,NF90_GLOBAL,'DX',&
+                  dx),&
+                  'Error in nf90_get_att: DX')
+             
+             call LIS_verify(nf90_get_att(ftn,NF90_GLOBAL,'DY',&
+                  dy),&
+                  'Error in nf90_get_att: DY')
+             
+             gridDesc = 0
+             
+             lat2 = (LISrunoffdata_struc(n)%nr-1)*dx + lat1
+             lon2 = (LISrunoffdata_struc(n)%nc-1)*dy + lon1
+             
+             gridDesc(1) = 0
+             gridDesc(2) = LISrunoffdata_struc(n)%nc
+             gridDesc(3) = LISrunoffdata_struc(n)%nr
+             gridDesc(4) = lat1
+             gridDesc(5) = lon1
+             gridDesc(7) = lat2
+             gridDesc(8) = lon2
+             gridDesc(6) = 128
+             gridDesc(9) = dx
+             gridDesc(10) = dy
+             gridDesc(20) = 64
+
+             
+             LISrunoffdata_struc(n)%datares = min(dx,dy)
+             
+             if(LIS_isAtAfinerResolution(n,LISrunoffdata_struc(n)%datares)) then
+                
+                allocate(LISrunoffdata_struc(n)%n11(LIS_rc%lnc(n)*LIS_rc%lnr(n)))
+                call neighbor_interp_input(n,gridDesc,&
+                     LISrunoffdata_struc(n)%n11)
+             else
+                
+                allocate(LISrunoffdata_struc(n)%n11(nc*nr))
+                call upscaleByAveraging_input(gridDesc,&
+                     LIS_rc%gridDesc(n,:),&
+                     nc*nr,&
+                     LIS_rc%lnc(n)*LIS_rc%lnr(n),&
+                     LISrunoffdata_struc(n)%n11)
+                
+             endif
+          endif
+       else
+          
+          write(LIS_logunit,*) '[ERR] currently only LIS data in lat/lon projection'
+          write(LIS_logunit,*) '[ERR] is supported'
+          call LIS_endrun()
+       
+       endif
+    enddo
+#endif    
     
     !ag - 17Mar2016
     do n=1, LIS_rc%nnest
