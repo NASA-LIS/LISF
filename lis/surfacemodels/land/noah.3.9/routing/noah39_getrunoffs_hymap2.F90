7cdcd5b01beb2e3179db0e9c6e4963b7399d8603
Brendan McAndrew <brendan.b.mcandrew@nasa.gov>
2022-03-28 16:51:08 -0400

    Update copyright notice

--- lis/surfacemodels/land/noah.3.9/routing/noah39_getrunoffs_hymap2.F90
+++ lis/surfacemodels/land/noah.3.9/routing/noah39_getrunoffs_hymap2.F90
@@ -1,7 +1,9 @@
 !-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
-! NASA Goddard Space Flight Center Land Information System (LIS) v7.2
+! NASA Goddard Space Flight Center
+! Land Information System Framework (LISF)
+! Version 7.3
 !
-! Copyright (c) 2015 United States Government as represented by the
+! Copyright (c) 2020 United States Government as represented by the
 ! Administrator of the National Aeronautics and Space Administration.
 ! All Rights Reserved.
 !-------------------------END NOTICE -- DO NOT EDIT-----------------------
