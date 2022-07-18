86f6c9e81f810088dc21785bd0237a5c04d4b39f
James V. Geiger <james.v.geiger@nasa.gov>
2022-07-18 15:40:04 -0400

    Remove multiple uses of ESMF within module files

--- lis/dataassim/obs/WUS_UCLAsnow/WUS_UCLAsnowMod.F90
+++ lis/dataassim/obs/WUS_UCLAsnow/WUS_UCLAsnowMod.F90
@@ -73,7 +73,6 @@ contains
 ! !INTERFACE: 
   subroutine WUS_UCLAsnow_setup(k, OBS_State, OBS_Pert_State)
 ! !USES: 
-    use ESMF
     use LIS_coreMod
     use LIS_timeMgrMod
     use LIS_historyMod
