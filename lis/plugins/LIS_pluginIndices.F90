49e6d1cb7ccd9a700dde7adb8a9126580d9179b0
Eric Kemp <eric.kemp@nasa.gov>
2022-11-03 15:56:39 -0400

    Fixed syntax error.

--- lis/plugins/LIS_pluginIndices.F90
+++ lis/plugins/LIS_pluginIndices.F90
@@ -173,7 +173,7 @@ module LIS_pluginIndices
    character*50, public,  parameter :: LIS_era5Id            = "ERA5"
    character*50, public,  parameter :: LIS_plumber2Id        = "PLUMBER2"
    character*50, public,  parameter :: LIS_gddpId            = "GDDP"
-   character*50, public,  parameter :: LIS_galwemId          = "GALWEM forecast
+   character*50, public,  parameter :: LIS_galwemId          = "GALWEM forecast"
    character*50, public,  parameter :: LIS_galwemgeId        = "GALWEM-GE forecast"
 
 !-------------------------------------------------------------------------
