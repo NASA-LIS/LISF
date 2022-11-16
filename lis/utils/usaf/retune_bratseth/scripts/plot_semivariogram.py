8b30683fbcbaa823b268225b0a9990de3b859b80
Eric Kemp <eric.kemp@nasa.gov>
2022-11-16 17:12:57 -0500

    More changes to placate pylint.

--- lis/utils/usaf/retune_bratseth/scripts/plot_semivariogram.py
+++ lis/utils/usaf/retune_bratseth/scripts/plot_semivariogram.py
@@ -63,66 +63,70 @@ def read_param_file(paramfile):
     return sigma2_gage, sigma2_back, L_back
 
 #------------------------------------------------------------------------------
-# Check command line
-if len(sys.argv) != 3:
-    print("[ERR] Bad command line arguments!")
-    usage()
-    sys.exit(1)
 
-# Read config file
-cfgfile = sys.argv[1]
-if not os.path.exists(cfgfile):
-    print(f"[ERR] Config file {cfgfile} does not exist!")
-    sys.exit(1)
-config = configparser.ConfigParser()
-config.read(cfgfile)
+def main():
 
-vario_filename, max_distance = semivar.read_input_section_cfg(config)
-function_type = semivar.read_fit_section_cfg(config)
-title, xlabel, ylabel, oblabel, bglabel = \
-    semivar.read_plot_section_cfg(config)
+    # Check command line
+    if len(sys.argv) != 3:
+        print("[ERR] Bad command line arguments!")
+        usage()
+        sys.exit(1)
 
-# Get the param file
-paramfile = sys.argv[2]
-if not os.path.exists(paramfile):
-    print(f"[ERR] Paramfile {paramfile} does not exist!")
-    sys.exit(1)
+    # Read config file
+    cfgfile = sys.argv[1]
+    if not os.path.exists(cfgfile):
+        print(f"[ERR] Config file {cfgfile} does not exist!")
+        sys.exit(1)
+    config = configparser.ConfigParser()
+    config.read(cfgfile)
 
-# Read the datafile
-distvector, variovector, samplesize = \
-    semivar.readdata(vario_filename, max_distance)
+    vario_filename, max_distance = semivar.read_input_section_cfg(config)
+    function_type = semivar.read_fit_section_cfg(config)
+    title, xlabel, ylabel, oblabel, bglabel = \
+        semivar.read_plot_section_cfg(config)
 
-# Read the paramfile
-sigma2_gage, sigma2_back, L_back = read_param_file(paramfile)
-popt = [sigma2_gage, sigma2_back, L_back]
+    # Get the param file
+    paramfile = sys.argv[2]
+    if not os.path.exists(paramfile):
+        print(f"[ERR] Paramfile {paramfile} does not exist!")
+        sys.exit(1)
 
-# Plot the semivariogram
-distvector_tmp = np.array([0])
-distvector     = np.concatenate((distvector_tmp, distvector))
+    # Read the datafile
+    distvector, variovector, samplesize = \
+         semivar.readdata(vario_filename, max_distance)
 
-variovector_tmp = np.array([np.nan])
-variovector = np.concatenate((variovector_tmp, variovector))
+    # Read the paramfile
+    sigma2_gage, sigma2_back, L_back = read_param_file(paramfile)
+    popt = [sigma2_gage, sigma2_back, L_back]
 
-fit_func = semivar.fit_func_dict[function_type]
-plt.plot(distvector, variovector, "b+",
-         distvector, fit_func(distvector, *popt), "r")
+    # Plot the semivariogram
+    distvector_tmp = np.array([0])
+    distvector     = np.concatenate((distvector_tmp, distvector))
 
-# Annotate
-fulltitle  = f"{title}\n"
-fulltitle += f"Based on {samplesize} comparisons of innovations\n"
-plt.title(fulltitle)
-plt.xlabel(f"{xlabel}")
-plt.ylabel(f"{ylabel}")
+    variovector_tmp = np.array([np.nan])
+    variovector = np.concatenate((variovector_tmp, variovector))
 
-plt.legend(["Data", f"{function_type} Best Fit"],
-           loc='lower right')
+    fit_func = semivar.fit_func_dict[function_type]
+    plt.plot(distvector, variovector, "b+",
+             distvector, fit_func(distvector, *popt), "r")
 
-params = r"$\sigma_{" + f"{oblabel}" + r"}^2 = " + f"{sigma2_gage:f},"
-params += r" \sigma_{" + f"{bglabel}" + r"}^2=" + f"{sigma2_back:f}"
-params += r", L_{" + f"{bglabel}" + r"} = " + f"{L_back:f}" + r"$"
+    # Annotate
+    fulltitle  = f"{title}\n"
+    fulltitle += f"Based on {samplesize} comparisons of innovations\n"
+    plt.title(fulltitle)
+    plt.xlabel(f"{xlabel}")
+    plt.ylabel(f"{ylabel}")
 
-print(params)
+    plt.legend(["Data", f"{function_type} Best Fit"],
+               loc='lower right')
 
-plt.figtext(0.2, 0.9, params)
-plt.grid(True)
-plt.show()
+    params = r"$\sigma_{" + f"{oblabel}" + r"}^2 = " + f"{sigma2_gage:f},"
+    params += r" \sigma_{" + f"{bglabel}" + r"}^2=" + f"{sigma2_back:f}"
+    params += r", L_{" + f"{bglabel}" + r"} = " + f"{L_back:f}" + r"$"
+
+    plt.figtext(0.2, 0.9, params)
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
