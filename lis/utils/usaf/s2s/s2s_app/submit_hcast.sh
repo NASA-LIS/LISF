9741701166d5356581700234cc4b0ae5449c6fcd
Sarith Mahanama <sarith.p.mahanama@nasa.gov>
2023-04-17 10:47:06 -0400

    changed to 2 step submitting: 1) BCSD, 2) FCST -> S@SPOST

--- lis/utils/usaf/s2s/s2s_app/submit_hcast.sh
+++ lis/utils/usaf/s2s/s2s_app/submit_hcast.sh
@@ -2,6 +2,7 @@
 
 MONTH=$1
 CFILE=$2
+BCSD=$3
 NJOBS=`qstat -u $USER | grep $USER | wc -l`
 clim_syr=`grep clim_start_year  $CFILE | cut -d':' -f2 | tr -d "[:space:]"`
 clim_eyr=`grep clim_end_year  $CFILE | cut -d':' -f2 | tr -d "[:space:]"`
@@ -13,19 +14,50 @@ else
     MM=$MONTH
 fi
 
-if [ -e current_year_$MM ]; then
-    echo "last completed year: " `cat current_year_$MM`
+current_year=hindcast/scratch/$MM/current_year_$MM
+bcsd_year=hindcast/scratch/$MM/bcsd_year_$MM
+NJMAX_BCSD=196
+NJMAX_FCST=164
+
+if [[ $BCSD == "BCSD" ]]; then
+    if [ -e $bcsd_year ]; then
+	echo "last completed year: " `cat $bcsd_year`
+    else
+	echo "missing " $bcsd_year
+	echo $clim_syr > $bcsd_year
+    fi
+    year=`cat $bcsd_year`
+    update_file=$bcsd_year
+    NJMAX=$NJMAX_BCSD
 else
-    echo "missing " current_year_$MM
-    echo $clim_syr > current_year_$MM
+    if [ -e $current_year ]; then
+	echo "last completed year: " `cat $current_year`
+    else
+	echo "missing " $current_year
+	echo $clim_syr > $current_year
+    fi
+    year=`cat $current_year`
+    update_file=$current_year
+    NJMAX=$NJMAX_FCST
 fi
 
-year=`cat current_year_$MM`
+if [ $NJOBS -gt $NJMAX ]; then
+    echo "Currently, " $NJOBS " are in the queue, please try later." 
+    exit
+fi
+
+# submit jobs
+
 ((year++))
 
-if [ $NJOBS -lt 130 ] && [ $year -le $clim_eyr ]; then
-    # NCCS permits 256 jobs per user at a given time. The s2s_run.sh (hindcast) consists of `100 jobs  
-    s2s_app/s2s_run.sh -y $year -m $MONTH -c $CFILE
-    echo $year > current_year_$MM
+if [ $NJOBS -le $NJMAX ] && [ $year -le $clim_eyr ]; then
+    # NCCS permits 256 jobs per user at a given time. The s2s_run.sh (hindcast) consists of `100 jobs
+    if [[ $BCSD == "BCSD" ]]; then
+	s2s_app/s2s_run.sh -y $year -m $MONTH -c $CFILE -s BCSD -o Y
+	echo $year > $update_file
+    else
+	s2s_app/s2s_run.sh -y $year -m $MONTH -c $CFILE -s FCST
+	echo $year > $update_file
+    fi
 fi
-chmod 777 current_year_$MM
+chmod 664 $update_file
