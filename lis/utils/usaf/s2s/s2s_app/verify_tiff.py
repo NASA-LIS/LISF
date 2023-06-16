c666e4f762fa46805b770b5ef6fd95dd43bd0dbc
James V. Geiger <james.v.geiger@nasa.gov>
2023-06-16 14:26:50 -0400

    Add NASA copyright notice

--- lis/utils/usaf/s2s/s2s_app/verify_tiff.py
+++ lis/utils/usaf/s2s/s2s_app/verify_tiff.py
@@ -2,6 +2,16 @@
 This script computes median across all model ensembles for each lead month separately and compares against the S2S produced TIF file.
 '''
 
+#-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+# NASA Goddard Space Flight Center
+# Land Information System Framework (LISF)
+# Version 7.4
+#
+# Copyright (c) 2022 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+#-------------------------END NOTICE -- DO NOT EDIT-----------------------
+
 import sys
 import argparse
 from datetime import date
--- lis/utils/usaf/s2s/s2s_modules/s2splots/plot_anom_verify.py
+++ lis/utils/usaf/s2s/s2s_modules/s2splots/plot_anom_verify.py
@@ -2,6 +2,16 @@
 This script derives monthly climatgology of the METFORCE uses in the LIS-DA step.
 '''
 
+#-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+# NASA Goddard Space Flight Center
+# Land Information System Framework (LISF)
+# Version 7.4
+#
+# Copyright (c) 2022 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+#-------------------------END NOTICE -- DO NOT EDIT-----------------------
+
 import os
 import sys
 import glob
