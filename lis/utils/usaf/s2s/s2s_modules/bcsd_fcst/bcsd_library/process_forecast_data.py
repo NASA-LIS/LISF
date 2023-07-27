added to handle 5D variables in nctool.py; restricted the search windown to 3x3; a few bug fixes to LDT masking of CFSv2 and NMME update

lis/utils/usaf/s2s/s2s_modules/bcsd_fcst/bcsd_library/process_forecast_data.py
@@ -164,9 +164,47 @@ def _migrate_to_monthly_files(cfsv2, outdirs, fcst_init, args, reg_precip):
                                                      var_name = "WIND")
     # get LDT mask LANDMASK
     ldt_xr = xr.open_dataset(args["ldtfile"])
+    mask_2d = np.array(ldt_xr['LANDMASK'].values)
+    mask_exp = mask_2d[np.newaxis,:,:]
+    darray = np.array(ds_out2['PRECTOT'].values)
+    mask = np.broadcast_to(mask_exp, darray.shape)
+
+    # apply mask
     ds_out2_masked = ds_out2.copy()
-    for da_name, da in ds_out2.data_vars.items():
-        ds_out2_masked[da_name] = xr.where(ldt_xr['LANDMASK'] == 0, -9999, da)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['PRECTOT'].values = darray
+
+    darray = np.array(ds_out2['PS'].values)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['PS'].values = darray
+
+    darray = np.array(ds_out2['T2M'].values)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['T2M'].values = darray
+
+    darray = np.array(ds_out2['LWS'].values)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['LWS'].values = darray
+
+    darray = np.array(ds_out2['SLRSF'].values)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['SLRSF'].values = darray
+
+    darray = np.array(ds_out2['Q2M'].values)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['Q2M'].values = darray
+
+    darray = np.array(ds_out2['U10M'].values)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['U10M'].values = darray
+
+    darray = np.array(ds_out2['V10M'].values)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['V10M'].values = darray
+
+    darray = np.array(ds_out2['WIND10M'].values)
+    darray[mask == 0] = -9999.
+    ds_out2_masked['WIND10M'].values = darray
     
     for month in range(1,10):
         file_6h = outdir_6hourly + '/' + final_name_pfx + '{:04d}{:02d}.nc'.format (dt1.year,dt1.month)