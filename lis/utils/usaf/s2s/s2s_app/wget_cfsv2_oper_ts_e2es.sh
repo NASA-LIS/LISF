bb23cfc551384f86dc4873eb8dc87e3242dc5cb5
Sarith Mahanama <sarith.p.mahanama@nasa.gov>
2023-08-01 08:17:47 -0400

    added the copyright notice to bash scripts

--- lis/utils/usaf/s2s/s2s_app/wget_cfsv2_oper_ts_e2es.sh
+++ lis/utils/usaf/s2s/s2s_app/wget_cfsv2_oper_ts_e2es.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 #
+
+#-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+# NASA Goddard Space Flight Center
+# Land Information System Framework (LISF)
+# Version 7.4
+# 
+# Copyright (c) 2022 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# -------------------------END NOTICE -- DO NOT EDIT-----------------------
+
 #  Title:  CFSv2 Operational Timeseries (Oper_TS) Download Script
 #
 #  Source:  NOAA NCEI Data Archive Center
