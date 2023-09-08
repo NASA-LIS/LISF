7ca39955373029759e18cdb45b5ee07248e40c91
Eric Kemp <eric.kemp@nasa.gov>
2023-09-08 16:52:09 -0400

    First attempt at adding IMERG Monthly support.
    This only supports V07A Final Run.
    
    Code compiles, but has not been run yet.

--- lvt/plugins/LVT_pluginIndices.F90
+++ lvt/plugins/LVT_pluginIndices.F90
@@ -262,6 +262,8 @@ module LVT_pluginIndices
    character*50, public,  parameter :: LVT_GDASdataId = "GDAS"
    character*50, public,  parameter :: LVT_ASOSWEdataId = "ASO SWE"
    character*50, public,  parameter :: LVT_IMERGdataId = "GPM IMERG"
+   character*50, public,  parameter :: LVT_IMERGMonthlydataId = &
+        "GPM IMERG Monthly"
    character*50, public,  parameter :: LVT_UASNOWdataId = "UA SNOW"
    character*50, public,  parameter :: LVT_ozFluxdataId = "OzFlux"
    character*50, public,  parameter :: LVT_JASMINsmobsId = "JASMIN soil moisture"
