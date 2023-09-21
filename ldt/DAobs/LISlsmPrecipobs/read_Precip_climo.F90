4bf03adf0b39d8d2699383c29a0e7cf25624a844
Mahdi Navari <mahdi.navari@nasa.gov>
2023-09-21 12:47:45 -0400

    Cleaning up the codes.

--- ldt/DAobs/LISlsmPrecipobs/read_Precip_climo.F90
+++ ldt/DAobs/LISlsmPrecipobs/read_Precip_climo.F90
@@ -28,15 +28,9 @@ subroutine read_Precip_climo(ncol, nrow, filename, precip)
 
      implicit none
 ! !ARGUMENTS:      
-!     integer,   intent(in)    :: n
-!     integer,   intent(in)    :: k
-!     integer,   intent(in)    :: nbins
-!     integer,   intent(in)    :: ntimes
      integer,   intent(in)    :: ncol, nrow
      character(len=*), intent(in)  :: filename
-!     character(len=*)         :: varname
      real                     :: precip(ncol,nrow,12)
-     !real        :: cdf(ngrid,ntimes, nbins)
      logical                  :: file_exists
 ! 
 ! !DESCRIPTION: 
@@ -54,13 +48,7 @@ subroutine read_Precip_climo(ncol, nrow, filename, precip)
 !  \item[cdf]           y-axis (CDF) values corresponding to the CDF
 ! \end{description}
 !EOP
-    ! integer                  :: j,kk
-    ! integer                  :: ngridId, nbinsId, nlevsId, ntimesId
-    ! integer                  :: ngrid_file, nbins_file, nlevs_file, ntimes_file
-    ! integer                  :: xid, cdfid 
      real, allocatable        :: precip_climo(:,:,:)
-     !real, allocatable        :: P_month(:,:)
-     !integer                  :: nid
      integer                  :: ios,nid,ncId,nrId,varId
      integer                  :: nc,nr,i,c,r
      character*3              :: month_name(12)
@@ -78,10 +66,8 @@ subroutine read_Precip_climo(ncol, nrow, filename, precip)
         call LDT_endrun()     
      end if
 
-  !if(file_exists) then
 
      write(LDT_logunit,*) '[INFO] Reading Precipitation climatology from CDF file ',trim(filename)
-     !if(ngrid.gt.0) then
         call LDT_verify(nf90_open(path=trim(filename),mode=NF90_NOWRITE,&
              ncid=nid),'failed to open file '//trim(filename))
 
@@ -107,7 +93,6 @@ subroutine read_Precip_climo(ncol, nrow, filename, precip)
         call LDT_endrun()
      endif
 
-    ! allocate(P_month(nc,nr))
      allocate(precip_climo(nc,nr,12))
 
      do i=1,12     
@@ -121,92 +106,9 @@ subroutine read_Precip_climo(ncol, nrow, filename, precip)
      ios = nf90_close(nid)
      call LDT_verify(ios,'Error in nf90_close in readldtparam_real_2d')
      write(LDT_logunit,*) '[INFO] Successfully read Precipitation climo file ',trim(filename)
-   !endif
 
    precip = precip_climo
    deallocate(precip_climo)
-
-
-#if 0 
-        call LDT_verify(nf90_inq_dimid(nid,"ngrid",ngridId), &
-             'nf90_inq_dimid failed for ngrid')
-        call LDT_verify(nf90_inq_dimid(nid,"nbins",nbinsId), &
-             'nf90_inq_dimid failed for nbins')
-        call LDT_verify(nf90_inq_dimid(nid,trim(varname)//"_levels",nlevsId), &
-             'nf90_inq_dimid failed for '//trim(varname)//"_levels")
-        call LDT_verify(nf90_inq_dimid(nid,"ntimes",ntimesId), &
-             'nf90_inq_dimid failed for ntimes')
-
-
-        call LDT_verify(nf90_inquire_dimension(nid,ngridId, len=ngrid_file),&
-             'nf90_inquire_dimension failed for ngrid')
-        call LDT_verify(nf90_inquire_dimension(nid,nbinsId, len=nbins_file),&
-             'nf90_inquire_dimension failed for nbins')
-        call LDT_verify(nf90_inquire_dimension(nid,nlevsId, len=nlevs_file),&
-             'nf90_inquire_dimension failed for nbins')
-        call LDT_verify(nf90_inquire_dimension(nid,ntimesId, len=ntimes_file),&
-             'nf90_inquire_dimension failed for ntimes')
-
-        !if(nbins.ne.nbins_file) then
-        !   write(LDT_logunit,*) '[ERR] The number of bins specified in the file '//&
-        !        trim(filename)
-        !   write(LDT_logunit,*) '[ERR] (',nbins_file, &
-        !        ') is different from the number of bins specified'
-        !   write(LDT_logunit,*) '[ERR] in the lis.config file (',nbins,')'
-        !   call LDT_endrun()
-        !endif
-        
-        if (ntimes_file .gt. 1) then 
-            write(LDT_logunit,*) '[ERR] The number of times specified in the file '//&
-                trim(filename)
-           write(LDT_logunit,*) '[ERR] (',ntimes_file, &
-                ') should be 1 set the Temporal resolution of precipitation CDFs to "yearly" '
-           call LDT_endrun()
-        endif
-
-        !allocate(xrange(ngrid_file,2))
-        allocate(xrange_file(ngrid_file,nlevs_file, nbins_file))
-        !allocate(cdf_file(ngrid_file,nlevs_file, nbins))
-
-        do j=1,ntimes_file
-           call LDT_verify(nf90_inq_varid(nid,trim(varname)//'_xrange',xid),&
-                'nf90_inq_varid failed for for '//trim(varname)//'_xrange')
-           !call LDT_verify(nf90_inq_varid(nid,trim(varname)//'_CDF',cdfid),&
-           !     'nf90_inq_varid failed for '//trim(varname)//'_CDF')
-
-           call LDT_verify(nf90_get_var(nid,xid,xrange_file, &
-                start=(/1,j,1,1/), count=(/ngrid_file,1,nlevs_file,nbins_file/)),&
-                'nf90_get_var failed for '//trim(varname)//'_xrange')
-           !call LDT_verify(nf90_get_var(nid,cdfid,cdf_file,&
-           !     start=(/1,j,1,1/), count=(/ngrid_file,1,nlevs_file,nbins/)),&
-           !     'nf90_get_var failed for '//trim(varname)//'_CDF')
-
-! Note: CDF is generated by LDT --> therefore data is in the LDT domain
-#if 0 
-           if(ngrid.gt.0) then
-              do kk=1,nbins
-                 call LIS_convertObsVarToLocalSpace(n,k,xrange_file(:,1,kk), &
-                      xrange(:,j,kk))
-                 call LIS_convertObsVarToLocalSpace(n,k,cdf_file(:,1,kk), &
-                      cdf(:,j,kk))
-              enddo
-           endif
-#endif  
-        enddo
-
-        xrange(:,1) = xrange_file(:,1,1) ! min value for each gridcell
-        xrange(:,2) = xrange_file(:,1,nbins_file) ! max value for each gridcell
-!        xrange = xrange_file
-!        cdf = cdf_file
-
-        deallocate(xrange_file)
-        !deallocate(cdf_file)
-
-        call LDT_verify(nf90_close(nid),&
-             'failed to close file '//trim(filename))
-        write(LDT_logunit,*) '[INFO] Successfully read CDF file ',trim(filename)
-     !endif
-#endif  
 
 ! end USE_NETCDF4
 #endif
