d8ce8195a9829e854c4f0c56e8e8082ddd020536
Eric Kemp <eric.kemp@nasa.gov>
2023-11-15 10:19:24 -0500

    Added [INFO] tag and trimmed character variable in write statement.

--- lis/surfacemodels/land/noah.3.9/noah39_readrst.F90
+++ lis/surfacemodels/land/noah.3.9/noah39_readrst.F90
@@ -102,13 +102,13 @@ subroutine noah39_readrst()
 
         if(.not.file_exists) then 
            write(LIS_logunit,*) '[ERR] Noah-3.9 restart file ',              &
-                noah39_struc(n)%rfile,' does not exist '
+                trim(noah39_struc(n)%rfile),' does not exist '
            write(LIS_logunit,*) '[ERR] Program stopping ...'
            call LIS_endrun()
            
         endif
         write(LIS_logunit,*)                        &
-             '[INFO] Noah-3.9 restart file used: ',noah39_struc(n)%rfile
+             '[INFO] Noah-3.9 restart file used: ',trim(noah39_struc(n)%rfile)
         
         if(wformat.eq."binary") then 
            ftn = LIS_getNextUnitNumber()
