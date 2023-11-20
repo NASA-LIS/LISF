10afaaca5cecc292dd9142b28c0b1d5a8e2e7cc3
David Mocko <David.Mocko@nasa.gov>
2023-11-20 15:25:53 -0500

    Fixing Noah-MP-4.0.1 LSM restart and model alarms for multiple nests in LIS
    This bug fix corrects the alarms for the Noah-MP-4.0.1 LSM in LIS
    when running multiple nests.  The restart alarm and the model alarm
    are both corrected.
    
    Resolves: #1457

--- lis/surfacemodels/land/noahmp.4.0.1/NoahMP401_lsmMod.F90
+++ lis/surfacemodels/land/noahmp.4.0.1/NoahMP401_lsmMod.F90
@@ -304,18 +304,18 @@ contains
 
             call LIS_update_timestep(LIS_rc, n, NOAHMP401_struc(n)%ts)
 
-            call LIS_registerAlarm("NoahMP401 model alarm",&
+            write(fnest,'(i3.3)') n
+            call LIS_registerAlarm("NoahMP401 model alarm "//trim(fnest),&
                                    NOAHMP401_struc(n)%ts, &
                                    NOAHMP401_struc(n)%ts)
 
-            call LIS_registerAlarm("NoahMP401 restart alarm", &
+            call LIS_registerAlarm("NoahMP401 restart alarm "//trim(fnest), &
                                    NOAHMP401_struc(n)%ts,&
                                    NOAHMP401_struc(n)%rstInterval)
             
             ! EMK Add alarm to reset tair_agl_min for RHMin.  This should 
             ! match the output interval, since that is used for calculating 
             ! Tair_F_min.            
-            write(fnest,'(i3.3)') n
             call LIS_registerAlarm("NoahMP401 RHMin alarm "//trim(fnest),&
                  NOAHMP401_struc(n)%ts,&
                  LIS_sfmodel_struc(n)%outInterval)
