69a8aa40770e0d2db6920751ba99cb1954eb839e
James V. Geiger <james.v.geiger@nasa.gov>
2023-11-21 08:59:31 -0500

    Remove extra "write(fnest)"

--- lis/surfacemodels/land/noahmp.4.0.1/NoahMP401_main.F90
+++ lis/surfacemodels/land/noahmp.4.0.1/NoahMP401_main.F90
@@ -1434,7 +1434,6 @@ subroutine NoahMP401_main(n)
 
     ! EMK...See if noahmp401_struc(n)%noahmp401(t)%tair_agl_min needs to be 
     ! reset for calculating RHMin.  
-    write(fnest,'(i3.3)') n
     alarmCheck = LIS_isAlarmRinging(LIS_rc, &
          "NoahMP401 RHMin alarm "//trim(fnest))
     if (alarmCheck) then
