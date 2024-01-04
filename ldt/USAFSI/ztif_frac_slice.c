bd4688482cc86ac64cd7dec206ea6c03b63273cf
James V. Geiger <james.v.geiger@nasa.gov>
2024-01-04 13:35:47 -0500

    Add copyright notice on LDT files for LISF 7.5.0 public release

--- ldt/USAFSI/ztif_frac_slice.c
+++ ldt/USAFSI/ztif_frac_slice.c
@@ -1,3 +1,12 @@
+//-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+// NASA Goddard Space Flight Center
+// Land Information System Framework (LISF)
+// Version 7.5
+//
+// Copyright (c) 2024 United States Government as represented by the
+// Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+//-------------------------END NOTICE -- DO NOT EDIT-----------------------
 
 #include "LDT_misc.h"
 #include "ftn_drv.h"
