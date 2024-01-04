99c62e71241a81ed9fb77c9390e5c4c5f046cddf
Eric Kemp <eric.kemp@nasa.gov>
2024-01-04 17:15:07 -0500

    Start of adding fault tolerance to LIS GALWEM reader.
    Not done yet.

--- lis/metforcing/galwem/get_galwem.F90
+++ lis/metforcing/galwem/get_galwem.F90
@@ -55,6 +55,10 @@ subroutine get_galwem(n, findex)
   integer           :: fcsthr_intv
   integer           :: fcst_hour
   integer           :: openfile
+  integer :: filecount
+  logical, save :: use_prior_galwem_run
+  integer :: cur_fcsthr, next_fcsthr
+  integer :: ierr
 
   ! GALWEM cycles every 6 hours; each cycle provide up to 168 hours (7 days) forecast for GALWEM-17km;
   ! each cycle provide up to 240 hours (10 days) forecast for GALWEM-25deg;
@@ -78,12 +82,84 @@ subroutine get_galwem(n, findex)
           '[INFO] LIS run has reached end time, will not read more GALWEM'
      return
   end if
-  
+
   openfile=0
 
   if(LIS_rc%tscount(n).eq.1 .or.LIS_rc%rstflag(n).eq.1) then  !beginning of run
      LIS_rc%rstflag(n) = 0
   endif
+
+  !EMK...Perform audit of available GALWEM-GD GRIB files for current fcst
+  !run.
+  if (LIS_rc%tscount(n) .eq. 1 .or. LIS_rc%rstflag(n) .eq. 1) then
+     use_prior_galwem_run = .false.
+     call run_audit(n, LIS_rc%syr, LIS_rc%smo, LIS_rc%sda, LIS_rc%shr, &
+          0, filecount)
+     if (filecount == 0) then !Roll back to prior GALWEM run
+        yr1 = LIS_rc%syr
+        mo1 = LIS_rc%smo
+        da1 = LIS_rc%sda
+        hr1 = LIS_rc%shr
+        mn1 = 0
+        ss1 = 0
+        ts1 = -43200
+        call LIS_tick(time1,doy1,gmt1,yr1,mo1,da1,hr1,mn1,ss1,ts1)
+        call run_audit(n, yr1, mo1, da1, hr1, &
+             12, filecount)
+        if (filecount == 0) then
+           write(LIS_logunit,*)"[ERR] No GALWEM files found!"
+           call LIS_endrun()
+        else
+           use_prior_galwem_run = .true.
+           galwem_struc(n)%init_yr = yr1
+           galwem_struc(n)%init_mo = mo1
+           galwem_struc(n)%init_da = da1
+           galwem_struc(n)%init_hr = hr1
+        end if
+     end if
+  end if
+  write(LIS_logunit,*) '[INFO] use_prior_galwem_run = ', &
+       use_prior_galwem_run
+
+  ! Get the bookends at the start of the run
+  if (LIS_rc%tscount(n) .eq. 1 .or. LIS_rc%rstflag(n) .eq. 1) then
+
+     ! Bookend-time record 1
+     yr1 = LIS_rc%syr
+     mo1 = LIS_rc%smo
+     da1 = LIS_rc%sda
+     hr1 = LIS_rc%shr
+     mn1 = 0
+     ss1 = 0
+     ts1 = 0
+     if (use_prior_galwem_run) ts1 = -43200
+     call LIS_tick(time1, doy1, gmt1, yr1, mo1, da1, hr1, mn1, ss1, ts1)
+
+     ! Find Bookend-time record 2
+     cur_fcsthr = 0
+     if (use_prior_galwem_run) cur_fcsthr = 12
+     call find_next_fcsthr(n, galwem_struc(n)%init_yr, &
+          galwem_struc(n)%init_mo, &
+          galwem_struc(n)%init_da, &
+          galwem_struc(n)%init_hr, &
+          cur_fcsthr, next_fcsthr, ierr)
+     if (ierr .ne. 0) then
+        write(LIS_logunit,*) '[ERR] Cannot find next GALWEM GRIB file!'
+        call LIS_endrun()
+     end if
+     yr2 = LIS_rc%syr
+     mo2 = LIS_rc%smo
+     da2 = LIS_rc%sda
+     hr2 = LIS_rc%shr
+     mn2 = 0
+     ss2 = 0
+     ts2 = next_fcsthr * 3600
+     if (use_prior_galwem_run) ts2 = ts2 - 43200
+     call LIS_tick(time2, doy2, gmt2, yr2, mo2, da2, hr2, mn2, ss2, ts2)
+
+     openfile = 1
+  end if
+  call LIS_endrun() ! EMK TEST
 
   ! First timestep of run
   if(LIS_rc%tscount(n).eq.1 .or.LIS_rc%rstflag(n).eq.1) then
@@ -276,4 +352,114 @@ subroutine getGALWEMfilename(n,rootdir,yr,mo,da,hr,fc_hr,filename)
              trim(fname) // ftime // '_CY.' // chr // '_FH.' // fchr // '_DF.GR2'
 end subroutine getGALWEMfilename
 
+! Run audit on GRIB files with specified start date and time.
+subroutine run_audit(n, yr, mo, da, hr, first_fcsthr, filecount)
 
+  ! Imports
+  use galwem_forcingMod, only: galwem_struc
+  use LIS_constantsMod, only: LIS_CONST_PATH_LEN
+  use LIS_logMod, only: LIS_logunit, LIS_endrun
+  use LIS_timeMgrMod, only: LIS_tick
+
+  ! Defaults
+  implicit none
+
+  ! Arguments
+  integer, intent(in) :: n
+  integer, intent(in) :: yr
+  integer, intent(in) :: mo
+  integer, intent(in) :: da
+  integer, intent(in) :: hr
+  integer, intent(in) :: first_fcsthr
+  integer, intent(out) :: filecount
+
+  ! Locals
+  integer, parameter :: MAX_FCSTHRS = 240
+  character(len=LIS_CONST_PATH_LEN) :: fname
+  integer :: fcsthr, delta
+  logical :: found_inq
+  integer :: i
+
+  ! Loop through list of forecast hours, and see how many files exist
+  delta = 1
+  filecount = 0
+  do i = 0, MAX_FCSTHRS
+     fcsthr = first_fcsthr + (i * delta)
+     call getGALWEMfilename(n, galwem_struc(n)%odir, yr, mo, da, hr, &
+          fcsthr, fname)
+     inquire(file=trim(fname), exist=found_inq)
+     if (found_inq) then
+        write(LIS_logunit,*)'[INFO] Found ', trim(fname)
+        filecount = filecount + 1
+     else
+        if (i == 0) then
+           write(LIS_logunit,*) &
+                '[WARN] Cannot find GALWEM GRIB file for LIS start date'
+           write(LIS_logunit,*) &
+                '[WARN] Will need to use earlier GALWEM run for forcing'
+           filecount = 0
+           exit
+        end if
+     end if
+  end do
+
+  if (filecount > 0) then
+     write(LIS_logunit,*)'[INFO] Found ', filecount, ' files'
+  end if
+  !yr1 = yr
+  !mo1 = mo
+  !da1 = da
+  !hr1 = hr
+  !mn1 = 0
+  !ss1 = 0
+  !ts1 = 0
+  !call LIS_tick(time1, doy1, gmt1, yr1, mo1, da1, hr1, mn1, ss1, ts1)
+
+end subroutine run_audit
+
+! Find next GALWEM grib file given current selected time and
+! forecast hour
+subroutine find_next_fcsthr(n, yr, mo, da, hr, cur_fcsthr, &
+     next_fcsthr, ierr)
+
+  ! Imports
+  use galwem_forcingMod, only: galwem_struc
+  use LIS_constantsMod, only: LIS_CONST_PATH_LEN
+  use LIS_logMod, only: LIS_logunit, LIS_endrun
+
+  ! Defaults
+  implicit none
+
+  ! Arguments
+  integer, intent(in) :: n
+  integer, intent(in) :: yr
+  integer, intent(in) :: mo
+  integer, intent(in) :: da
+  integer, intent(in) :: hr
+  integer, intent(in) :: cur_fcsthr
+  integer, intent(out) :: next_fcsthr
+  integer, intent(out) :: ierr
+
+  ! Locals
+  integer, parameter :: MAX_FCSTHRS = 240
+  character(len=LIS_CONST_PATH_LEN) :: fname
+  integer :: fcsthr, delta
+  logical :: found_inq
+  integer :: i
+
+  ! Loop through list of forecast hours, and see how many files exist
+  ierr = 1
+  delta = 1
+  do i = 1, MAX_FCSTHRS
+     fcsthr = cur_fcsthr + (i * delta)
+     call getGALWEMfilename(n, galwem_struc(n)%odir, yr, mo, da, hr, &
+          fcsthr, fname)
+     inquire(file=trim(fname), exist=found_inq)
+     if (found_inq) then
+        next_fcsthr = fcsthr
+        ierr = 0
+        exit
+     end if
+  end do
+
+end subroutine find_next_fcsthr
