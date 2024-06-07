eacddedc7433b1e602b5eaac5f705ef4aaf5441c
James V. Geiger <james.v.geiger@nasa.gov>
2024-06-07 10:40:20 -0400

    Add copyright notice to new files

--- lis/core/LIS_rcFileReading_mod.F90
+++ lis/core/LIS_rcFileReading_mod.F90
@@ -1,7 +1,13 @@
+!-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+! NASA Goddard Space Flight Center
+! Land Information System Framework (LISF)
+! Version 7.5
+!
+! Copyright (c) 2024 United States Government as represented by the
+! Administrator of the National Aeronautics and Space Administration.
+! All Rights Reserved.
+!-------------------------END NOTICE -- DO NOT EDIT-----------------------
 #include "LIS_misc.h"
-!------------------------------------------------------------------------------
-! NASA GSFC 
-!------------------------------------------------------------------------------
 !BOP
 !
 ! !MODULE: LIS_rcFileReading_mod
