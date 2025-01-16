e141a942b9930150be86432f585198937fde7127
Eric Kemp <eric.kemp@nasa.gov>
2025-01-16 14:05:19 -0500

    New code changes to use LVT_CONST_PATH_LEN.

--- lvt/datastreams/LVTbenchmarkOUT/LVTbenchmarkOUT_obsMod.F90
+++ lvt/datastreams/LVTbenchmarkOUT/LVTbenchmarkOUT_obsMod.F90
@@ -16,7 +16,9 @@
 module LVTbenchmarkOUT_obsMod
 ! 
 ! !USES:   
-!
+  !
+  use LVT_constantsMod, only: LVT_CONST_PATH_LEN
+
 ! !INPUT PARAMETERS: 
 ! 
 ! !OUTPUT PARAMETERS:
@@ -46,7 +48,7 @@ module LVTbenchmarkOUT_obsMod
 !EOP
   
   type, public :: daobsdec
-     character*100  :: odir
+     character(len=LVT_CONST_PATH_LEN) :: odir
      character*50   :: vname
   end type daobsdec
 
