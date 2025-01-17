18a9f3b9838ab1e2c2511e859bd55a67b5931d67
Eric Kemp <eric.kemp@nasa.gov>
2025-01-17 11:29:23 -0500

    More code commits to use LVT_CONST_PATH_LEN.

--- lvt/datastreams/Natural_streamflow/NatSF_obsMod.F90
+++ lvt/datastreams/Natural_streamflow/NatSF_obsMod.F90
@@ -17,6 +17,7 @@ module NatSF_obsMod
 ! 
 ! !USES:   
   use ESMF
+  use LVT_constantsMod, only: LVT_CONST_PATH_LEN
 
   implicit none
 
@@ -40,7 +41,7 @@ module NatSF_obsMod
 ! 
 !EOP
 
-     character*100           :: odir
+     character(len=LVT_CONST_PATH_LEN) :: odir
      integer                 :: n_stns
      character*100, allocatable  :: stn_name(:)
      real,          allocatable  :: stnlat(:)
