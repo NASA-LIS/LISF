ca6004e166024e2bd447833ea8ecf8b575869b27
Eric Kemp <eric.kemp@nasa.gov>
2025-01-31 16:52:52 -0500

    Code cleanup.

--- lis/rtms/CRTM2EM/pe/CRTM2EM_peMod.F90
+++ lis/rtms/CRTM2EM/pe/CRTM2EM_peMod.F90
@@ -60,11 +60,12 @@ contains
 ! !INTERFACE:
   subroutine crtm2em_setup_pedecvars(DEC_State, Feas_State)
 ! !USES:
-    use LIS_constantsMod,  only : LIS_CONST_PATH_LEN
+#if (defined RTMS) 
     use LIS_coreMod,       only : LIS_rc, LIS_config,LIS_vecPatch, LIS_surface, LIS_localPET
     use LIS_logMod,        only : LIS_logunit, LIS_verify
-#if (defined RTMS) 
+
     use CRTM2_EMMod,       only : crtm_struc
+    use LIS_constantsMod,  only : LIS_CONST_PATH_LEN
 #endif
     implicit none
 ! !ARGUMENTS: 
@@ -80,7 +81,7 @@ contains
 !EOP
 #if (defined RTMS)
     character(len=LIS_CONST_PATH_LEN) :: decSpaceAttribsFile
-#endif
+
     integer                     :: n 
     type(ESMF_ArraySpec)        :: arrspec1
     type(ESMF_Field)            :: varField
@@ -97,7 +98,6 @@ contains
     integer                     :: count
     integer                     :: gid
 
-#if (defined RTMS) 
     call ESMF_StateGet(Feas_State, "Feasibility Flag", feasField, rc=status)
     call LIS_verify(status)
     call ESMF_FieldGet(feasField,localDE=0,farrayPtr=mod_flag,rc=status)
