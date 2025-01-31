5c1bf6516f4c25bd3d124c07ee90a7247e69faa7
Eric Kemp <eric.kemp@nasa.gov>
2025-01-31 16:36:32 -0500

    Code cleanup.

--- lis/core/LIS_RTMMod.F90
+++ lis/core/LIS_RTMMod.F90
@@ -94,9 +94,6 @@ contains
     character*10            :: time
     character*100           :: temp
     integer                 :: status
-#if 0
-    character(len=LIS_CONST_PATH_LEN) :: statsfilename    
-#endif
 
     TRACE_ENTER("rtm_init")
     allocate(LIS_rtm_struc(LIS_rc%nnest))
@@ -170,16 +167,6 @@ contains
        
        call rtminitialize(trim(LIS_rc%rtm)//char(0))
     endif
-#if 0 
-!!!!!!!!!!!!!TESTING OUTPUT FILE  INITIALIZATION STEP (BEGIN)
-       write(unit=outputdirname,FMT='(I2.2)') trim(LIS_rc%rtm)
-       outputdirname='RTM_'//trim(outputdirname)
-       write(unit=outputfilename,FMT='(I2.2)') trim(LIS_rc%rtm)
-       outputfilename='RTM_'//trim(outputfilename)
-       write(unit=statsfilename,FMT='(I2.2)') trim(LIS_rc%rtm)
-       statsfilename='RTMStats_'//trim(statsfilename)
-       call LIS_create_stats_filename(n, fstatsname,statsfilename)
-#endif       
     TRACE_EXIT("rtm_init")
        
   end subroutine LIS_RTM_init
