7b972ddaba7950541134b6e39c3bcf0c226736da
Eric Kemp <eric.kemp@nasa.gov>
2025-02-28 12:02:03 -0500

    Updated indentation. Added use of LDT_CONST_PATH_LEN.

--- ldt/params/AquaCrop/read_CONSTANT_AC_crop.F90
+++ ldt/params/AquaCrop/read_CONSTANT_AC_crop.F90
@@ -18,93 +18,94 @@
 ! !INTERFACE:
 subroutine read_CONSTANT_AC_crop(n, array)
 
-! !USES:
+  ! !USES:
   use ESMF
+  use LDT_constantsMod, only: LDT_CONST_PATH_LEN
   use LDT_coreMod,      only : LDT_rc, LDT_config
   use LDT_paramDataMod, only: LDT_LSMparam_struc
   use LDT_logMod
 
   implicit none
-! !ARGUMENTS: 
+  ! !ARGUMENTS: 
   integer, intent(in) :: n
   real, intent(inout) :: array(LDT_rc%lnc(n),LDT_rc%lnr(n))
 
-! !LOCALS:
+  ! !LOCALS:
   integer        :: ftn, ios1
   integer        :: i, k, rc, num_types, crop_index
   integer        :: r,c
   character(100) :: croptype
-  character(200) :: dir
-  character(200) :: cropinv_file
+  character(len=LDT_CONST_PATH_LEN) :: dir
+  character(len=LDT_CONST_PATH_LEN) :: cropinv_file
   character(100) :: header1
   character(100) :: read_cropname
   character(100) :: read_fullname
 
-! !DESCRIPTION:
-!  This subroutine sets a constant crop type defined in the
-!  configuration file.
-!
-!  The arguments are:
-!  \begin{description}
-!  \item[n]
-!   index of the nest
-!  \item[array]
-!   output field with the crop type
-!  \end{description}
-!EOP
-! ____________________________
+  ! !DESCRIPTION:
+  !  This subroutine sets a constant crop type defined in the
+  !  configuration file.
+  !
+  !  The arguments are:
+  !  \begin{description}
+  !  \item[n]
+  !   index of the nest
+  !  \item[array]
+  !   output field with the crop type
+  !  \end{description}
+  !EOP
+  ! ____________________________
 
-!- Finds chosen crop type
+  !- Finds chosen crop type
   call ESMF_ConfigFindLabel(LDT_config,"AquaCrop crop type:",rc=rc)
   call ESMF_ConfigGetAttribute(LDT_Config, croptype, rc=rc)
   call LDT_verify(rc,"AquaCrop crop type: not defined")
 
-!- Finds crop inventory
+  !- Finds crop inventory
   call ESMF_ConfigFindLabel(LDT_config,"AquaCrop crop library directory:",rc=rc)
   call ESMF_ConfigGetAttribute(LDT_Config, dir,rc=rc)
   call LDT_verify(rc,"AquaCrop crop library directory: not defined")
 
-   cropinv_file = trim(dir)//"AC_Crop.Inventory"
+  cropinv_file = trim(dir)//"AC_Crop.Inventory"
 
-   write(LDT_logunit,*) "- Reading Crop Library File: ",&
-                         trim(cropinv_file)
+  write(LDT_logunit,*) "- Reading Crop Library File: ",&
+       trim(cropinv_file)
 
-!- Open file:
-   ftn = LDT_getNextUnitNumber()
-   open(ftn, file=cropinv_file, status='old', form='formatted',&
-        iostat=ios1)
-   call LDT_verify(ios1,"AquaCrop crop inventory does not exist")
+  !- Open file:
+  ftn = LDT_getNextUnitNumber()
+  open(ftn, file=cropinv_file, status='old', form='formatted',&
+       iostat=ios1)
+  call LDT_verify(ios1,"AquaCrop crop inventory does not exist")
 
-!- Read crop inventory file:
-   read(ftn,fmt=*) num_types
-   read(ftn,fmt=*) header1
-   do i = 1, num_types
-      read(ftn,fmt=*) k, read_cropname, read_fullname 
-      if( trim(croptype) == trim(read_cropname) ) then
+  !- Read crop inventory file:
+  read(ftn,fmt=*) num_types
+  read(ftn,fmt=*) header1
+  do i = 1, num_types
+     read(ftn,fmt=*) k, read_cropname, read_fullname 
+     if( trim(croptype) == trim(read_cropname) ) then
         crop_index = k    ! Crop_index assignment
         exit
-      endif
-      if ( (i.eq.num_types).and.(crop_index.ne.k) ) then
+     endif
+     if ( (i.eq.num_types).and.(crop_index.ne.k) ) then
         write(LDT_logunit,*) "[ERR] AC72: ", trim(croptype), &
-                             " not in AC_Crop.Inventory"
+             " not in AC_Crop.Inventory"
         call LDT_endrun
-      endif
-   end do
-   
-   call LDT_releaseUnitNumber(ftn)
+     endif
+  end do
 
-    ! fill the array with the crop type in case not masked
-    do r=1,LDT_rc%lnr(n)
-        do c=1,LDT_rc%lnc(n)
-          if (LDT_LSMparam_struc(n)%landmask%value(c,r,1).gt.0) then
-            array(c,r) = crop_index
-          else
-            array(c,r) = LDT_rc%udef
-          endif
-        enddo
-    enddo
+  call LDT_releaseUnitNumber(ftn)
+
+  ! fill the array with the crop type in case not masked
+  do r=1,LDT_rc%lnr(n)
+     do c=1,LDT_rc%lnc(n)
+        if (LDT_LSMparam_struc(n)%landmask%value(c,r,1).gt.0) then
+           array(c,r) = crop_index
+        else
+           array(c,r) = LDT_rc%udef
+        endif
+     enddo
+  enddo
 
   write(LDT_logunit, *) "[INFO] Setting a CONSTANT crop type value ..."
-  
+
 
 end subroutine read_CONSTANT_AC_crop
