544e26123811607f11372e338acb3988bca3e2c6
Eric Kemp <eric.kemp@nasa.gov>
2025-02-28 15:18:09 -0500

    Revised indentation. Removed unnecessary module imports.

--- lis/surfacemodels/land/ac.7.2/ac72_dynsetup.F90
+++ lis/surfacemodels/land/ac.7.2/ac72_dynsetup.F90
@@ -17,19 +17,13 @@
 !
 ! !INTERFACE:
 subroutine AC72_dynsetup(n)
-! !USES:
-    use LIS_logMod, only     : LIS_logunit
-    use LIS_coreMod, only    : LIS_rc, LIS_domain, LIS_surface
-    use LIS_timeMgrMod, only : LIS_date2time, LIS_tick
-    use LIS_fileIOMod, only  : LIS_read_param
-    use AC72_lsmMod, only : AC72_struc
-!
-! !DESCRIPTION:
-!  This routine sets up the time-dependent variables in AC72
-!
-!EOP
-    implicit none
-    integer, intent(in) :: n
- 
-    !TODO: add code here if needed.
+  !
+  ! !DESCRIPTION:
+  !  This routine sets up the time-dependent variables in AC72
+  !
+  !EOP
+  implicit none
+  integer, intent(in) :: n
+
+  !TODO: add code here if needed.
 end subroutine AC72_dynsetup
