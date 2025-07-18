be25926c37cd22d08bbca41329216851c29714e1
Eric Kemp <eric.kemp@nasa.gov>
2025-07-18 12:30:32 -0400

    First attempt at adding SNIP plugins for Noah39 and NoahMP401.

---
+++ lis/surfacemodels/land/noahmp.4.0.1/da_snip/noahmp401_descale_snip.F90
@@ -0,0 +1,51 @@
+!-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
+! NASA Goddard Space Flight Center
+! Land Information System Framework (LISF)
+! Version 7.5
+!
+! Copyright (c) 2024 United States Government as represented by the
+! Administrator of the National Aeronautics and Space Administration.
+! All Rights Reserved.
+!-------------------------END NOTICE -- DO NOT EDIT-----------------------
+!BOP
+! !ROUTINE: noahmp401_descale_snip
+! \label{noahmp401_descale_snip}
+!
+! !REVISION HISTORY:
+! 27Feb2005: Sujay Kumar; Initial Specification
+! 25Jun2006: Sujay Kumar: Updated for the ESMF design
+! 02 Mar 2010: Sujay Kumar; Modified for Noah 3.1
+! 14 Dec 2018: Yeosang Yoon; Modified for NoahMP 4.0.1 and SNODEP
+! 15 May 2019: Yeosang Yoon; Modified for NoahMP 4.0.1 and LDTSI
+! 13 Dec 2019: Eric Kemp; Replaced LDTSI with USAFSI
+! 18 Jul 2025: Eric Kemp; SNIP version
+
+!
+! !INTERFACE:
+subroutine noahmp401_descale_snip(n, LSM_State, LSM_Incr_State)
+
+  ! !USES:
+  use ESMF
+
+  ! Defaults
+  implicit none
+
+  ! !ARGUMENTS:
+  integer, intent(in)    :: n
+  type(ESMF_State)       :: LSM_State
+  type(ESMF_State)       :: LSM_Incr_State
+  !
+  ! !DESCRIPTION:
+  !
+  !  Returns the snow related state prognostic variables for
+  !  data assimilation
+  !
+  !  The arguments are:
+  !  \begin{description}
+  !  \item[n] index of the nest \newline
+  !  \item[LSM\_State] ESMF State container for LSM state variables \newline
+  !  \end{description}
+  !EOP
+
+end subroutine noahmp401_descale_snip
+
