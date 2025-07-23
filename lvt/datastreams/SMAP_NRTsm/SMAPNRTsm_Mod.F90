8d9fb1b097f26aeed504f3245b2012f446bbf571
Eric Kemp <eric.kemp@nasa.gov>
2025-07-23 09:40:12 -0400

    Minor cleanup.

--- lvt/datastreams/SMAP_NRTsm/SMAPNRTsm_Mod.F90
+++ lvt/datastreams/SMAP_NRTsm/SMAPNRTsm_Mod.F90
@@ -14,35 +14,40 @@
 !
 ! !INTERFACE:
 module SMAPNRTsm_Mod
-!
-! !USES:
+  !
+  ! !USES:
   use ESMF
 
   implicit none
 
   PRIVATE
-!
-! !DESCRIPTION:
-!  This module handles the observation plugin for the
-!  retrieval products from the Soil Moisture Active Passive
-!  (SMAP) mission
-!
-! !FILES USED:
-!
-! !REVISION HISTORY:
-!  2 July 2025: Mahdi Navari, Initial Specification
-!
-!EOP
+  !
+  ! !DESCRIPTION:
+  !  This module handles the observation plugin for the
+  !  retrieval products from the Soil Moisture Active Passive
+  !  (SMAP) mission
+  !
+  ! !FILES USED:
+  !
+  ! !REVISION HISTORY:
+  !  2 July 2025: Mahdi Navari, Initial Specification
+  !
+  !EOP
 
-!--------------------------------------------------------------------------
-! !PUBLIC MEMBER FUNCTIONS:
-!--------------------------------------------------------------------------
+  !------------------------------------------------------------------------
+  ! !PUBLIC MEMBER FUNCTIONS:
+  !------------------------------------------------------------------------
+
   PUBLIC :: SMAPNRT_smobsinit
-!--------------------------------------------------------------------------
-! !PUBLIC TYPES:
-!--------------------------------------------------------------------------
+
+  !------------------------------------------------------------------------
+  ! !PUBLIC TYPES:
+  !------------------------------------------------------------------------
+
   PUBLIC :: SMAPNRT_smobs
-!EOP
+
+  !EOP
+
   type, public :: smapobsdec
 
      character*255        :: odir
@@ -51,13 +56,13 @@ module SMAPNRTsm_Mod
      integer              :: nc
      integer              :: nr
      integer              :: mo
-     integer,allocatable  :: n112(:)
-     real,allocatable     :: rlat2(:)
-     real,allocatable     :: rlon2(:)
+     integer, allocatable :: n112(:)
+     real, allocatable    :: rlat2(:)
+     real, allocatable    :: rlon2(:)
      real                 :: gridDesci(50)
 
-     real,    allocatable    :: smobs(:,:)
-     real,    allocatable    :: smtime(:,:)
+     real, allocatable    :: smobs(:,:)
+     real, allocatable    :: smtime(:,:)
      integer*2, allocatable  :: smqc(:,:)
      logical                 :: startflag
 
@@ -67,40 +72,40 @@ module SMAPNRTsm_Mod
 
 contains
 
-!BOP
-!
-! !ROUTINE: SMAPNRT_smobsInit
-! \label{SMAPNRT_smobsInit}
-!
-! !INTERFACE:
+  !BOP
+  !
+  ! !ROUTINE: SMAPNRT_smobsInit
+  ! \label{SMAPNRT_smobsInit}
+  !
+  ! !INTERFACE:
   subroutine SMAPNRT_smobsinit(i)
-!
-! !USES:
+    !
+    ! !USES:
     use LVT_coreMod
     use LVT_histDataMod
     use LVT_logMod
     use LVT_timeMgrMod
 
     implicit none
-!
-! !INPUT PARAMETERS:
+    !
+    ! !INPUT PARAMETERS:
     integer,   intent(IN) :: i
-!
-! !OUTPUT PARAMETERS:
-!
-! !DESCRIPTION:
-!  This subroutine initializes and sets up the data structures required
-!  for reading SMAP L2 NRT soil moisture data.
-!
-! !FILES USED:
-!
-! !REVISION HISTORY:
-!
-!EOP
+    !
+    ! !OUTPUT PARAMETERS:
+    !
+    ! !DESCRIPTION:
+    !  This subroutine initializes and sets up the data structures
+    !  required for reading SMAP L2 NRT soil moisture data.
+    !
+    ! !FILES USED:
+    !
+    ! !REVISION HISTORY:
+    !
+    !EOP
 
-    integer            :: npts
-    integer                 :: ease_nc,ease_nr
-    integer                 :: status
+    integer :: npts
+    integer :: ease_nc, ease_nr
+    integer :: status
 
     external :: neighbor_interp_input
     external :: system
@@ -110,14 +115,15 @@ contains
     endif
 
     call ESMF_ConfigGetAttribute(LVT_Config, SMAPNRT_smobs(i)%odir, &
-         label='SMAP L2 NRT soil moisture observation directory:', rc=status)
+         label='SMAP L2 NRT soil moisture observation directory:', &
+         rc=status)
     call LVT_verify(status, &
          'SMAP L2 NRT soil moisture observation directory: not defined')
 
-    !SMAP L2  36km 
+    !SMAP L2 36km
     !filling the items needed by the interpolation library
-    ease_nc=964       
-    ease_nr=406
+    ease_nc = 964
+    ease_nr = 406
     SMAPNRT_smobs(i)%gridDesci = 0
     SMAPNRT_smobs(i)%gridDesci(1) = 9  !input is EASE grid
     SMAPNRT_smobs(i)%gridDesci(2) = ease_nc  !nx
@@ -128,26 +134,26 @@ contains
     SMAPNRT_smobs(i)%gridDesci(11) = 1
 
     SMAPNRT_smobs(i)%nc=ease_nc
-    SMAPNRT_smobs(i)%nr=ease_nr       
-       
+    SMAPNRT_smobs(i)%nr=ease_nr
+
     call LVT_update_timestep(LVT_rc, 3600)
-    
+
     npts= LVT_rc%lnc*LVT_rc%lnr
     SMAPNRT_smobs(i)%mo=npts
-    
+
     allocate(SMAPNRT_smobs(i)%rlat2(npts))
     allocate(SMAPNRT_smobs(i)%rlon2(npts))
     allocate(SMAPNRT_smobs(i)%n112(npts))
-    
-    SMAPNRT_smobs(i)%rlat2=0.0
-    SMAPNRT_smobs(i)%rlon2=0.0
-    SMAPNRT_smobs(i)%n112=0.0
-    call neighbor_interp_input(SMAPNRT_smobs(i)%gridDesci,LVT_rc%gridDesc,&
-         npts,SMAPNRT_smobs(i)%rlat2,&
-         SMAPNRT_smobs(i)%rlon2,SMAPNRT_smobs(i)%n112)
-    
 
-    SMAPNRT_smobs(i)%startflag = .true. 
+    SMAPNRT_smobs(i)%rlat2 = 0.0
+    SMAPNRT_smobs(i)%rlon2 = 0.0
+    SMAPNRT_smobs(i)%n112 = 0.0
+    call neighbor_interp_input(SMAPNRT_smobs(i)%gridDesci, &
+         LVT_rc%gridDesc, &
+         npts, SMAPNRT_smobs(i)%rlat2, &
+         SMAPNRT_smobs(i)%rlon2, SMAPNRT_smobs(i)%n112)
+
+    SMAPNRT_smobs(i)%startflag = .true.
 
     allocate(SMAPNRT_smobs(i)%smobs(LVT_rc%lnc*LVT_rc%lnr,2))
     allocate(SMAPNRT_smobs(i)%smtime(LVT_rc%lnc,LVT_rc%lnr))
