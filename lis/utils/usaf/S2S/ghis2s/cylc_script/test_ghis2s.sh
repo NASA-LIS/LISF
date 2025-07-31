723b18cf81d20ee351820324fd9f2d2fc7cb031e
Sarith Mahanama <sarith.p.mahanama@nasa.gov>
2025-07-31 07:58:00 -0400

    exit is SUBMIT_JOB

--- lis/utils/usaf/S2S/ghis2s/cylc_script/test_ghis2s.sh
+++ lis/utils/usaf/S2S/ghis2s/cylc_script/test_ghis2s.sh
@@ -3,17 +3,18 @@
 # Set required environment variables
 export CONFIG_FILE="s2s_config_global_fcast"
 export FORECAST_YEAR=2025
-export FORECAST_MONTH=1
-export USER_EMAIL="sarith.p.mahanama@nasa.gov"
-export E2ESDIR="/discover/nobackup/projects/ghilis/smahanam/ghi-coupling/"
-export OUTPUT_ROOT="/discover/nobackup/projects/ghilis/smahanam/GHI-repos/s2s_workflows/"
+export FORECAST_MONTH=3
+export USER_EMAIL="kristi.r.arsenault@nasa.gov"
+#export USER_EMAIL="sarith.p.mahanama@nasa.gov"
+export E2ESDIR="/discover/nobackup/projects/ghilis/S2S/GLOBAL/cylc_test1/"
+export OUTPUT_ROOT="/discover/nobackup/projects/ghilis/S2S/GHI-repos/s2s_workflows/"
 export LISFDIR=`grep LISFDIR $E2ESDIR/$CONFIG_FILE | cut -d':' -f2 | tr -d "[:space:]"`
 export LISFMOD=`grep LISFMOD $E2ESDIR/$CONFIG_FILE | cut -d':' -f2 | tr -d "[:space:]"`
 export PYTHONPATH="${LISFDIR}/lis/utils/usaf/S2S/"
 mkdir -p -m 775 $OUTPUT_ROOT
 
 # Optional variables
-export S2S_STEP="E2E"
+export S2S_STEP="POST"
 export ONE_STEP=false
 export SUBMIT_JOB=false
 
@@ -23,8 +24,11 @@ module use -a "${LISFDIR}/env/discover/"
 module --ignore-cache load $LISFMOD
 
 # Run S2S Python program
-cd /discover/nobackup/projects/ghilis/smahanam/GHI-repos/ghi-apps/bin/
+cd /discover/nobackup/projects/ghilis/S2S/GHI-repos/ghi-apps/bin/
 python ghis2s_program.py
+
+# if SUBMIT JOBS to SLURM exit
+[ "$SUBMIT_JOB" = "true" ] && exit
 
 # Run Cylc
 module purge
@@ -53,11 +57,18 @@ echo "==========================================================================
 echo "Useful CYLC commands from ${CWD}"
 echo "======================================================================================================="
 echo
+echo "First load Cylc module"
+echo
+echo "setenv USE_CYLC_ENV 1"
+echo "module use -a ${LISFDIR}/env/discover/"
+echo "module --ignore-cache load $LISFMOD"
+echo
 echo "Run ${WORKFLOW_NAME}: cylc play ${WORKFLOW_NAME}"
 echo "Monitor ${WORKFLOW_NAME}: cylc tui ${WORKFLOW_NAME}"
 echo "Show status ${WORKFLOW_NAME}: cylc show ${WORKFLOW_NAME}"
 echo "Stop ${WORKFLOW_NAME}: cylc stop --now ${WORKFLOW_NAME}"
 echo "Cat log: cylc cat-log ${WORKFLOW_NAME}"
-
+echo
+echo "You might want to module purge and unsetenv USE_CYLC_ENV after running above Cylc commands." 
 
 
