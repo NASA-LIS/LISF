3826446c7f0e980ea094f68f88e08f7bca896f44
Eric Kemp <eric.kemp@nasa.gov>
2025-09-18 15:09:08 -0400

    Code cleanup.

--- ldt/params/HYMAP/read_HYMAP_urban_drain_outlet.F90
+++ ldt/params/HYMAP/read_HYMAP_urban_drain_outlet.F90
@@ -20,14 +20,14 @@
 subroutine read_HYMAP_urban_drain_outlet(n, array)
 ! !USES:
   use ESMF
+  use HYMAP_parmsMod
   use LDT_coreMod,      only : LDT_rc
+  use LDT_fileIOMod,    only : readLISdata
   use LDT_logMod,       only : LDT_logunit, LDT_getNextUnitNumber, &
           LDT_releaseUnitNumber, LDT_endrun
-  use LDT_fileIOMod,    only : readLISdata 
-  use HYMAP_parmsMod
 
   implicit none
-! !ARGUMENTS: 
+! !ARGUMENTS:
 
   integer,          intent(in) :: n
   real,          intent(inout) :: array(LDT_rc%lnc(n),LDT_rc%lnr(n),1)
@@ -39,14 +39,15 @@ subroutine read_HYMAP_urban_drain_outlet(n, array)
   ftn = LDT_getNextUnitNumber()
 
   inquire(file=trim(HYMAP_struc(n)%urbandrainoutletfile), exist=file_exists)
-  if(.not.file_exists) then 
+  if (.not.file_exists) then
      write(LDT_logunit,*) '[ERR] HYMAP urban drainage outlet map, ',&
            trim(HYMAP_struc(n)%urbandrainoutletfile),', not found.'
      write(LDT_logunit,*) 'Program stopping ...'
      call LDT_endrun
   endif
 
-  open(ftn, file=trim(HYMAP_struc(n)%urbandrainoutletfile), access='direct',&
+  open(ftn, file=trim(HYMAP_struc(n)%urbandrainoutletfile), &
+       access='direct', &
        status='old', form="unformatted", convert="big_endian", recl=4)
 
   call readLISdata(n, ftn, HYMAP_struc(n)%hymap_proj, &
