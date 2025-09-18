929795be333a0100d04a1fee3b63b16836bb6c8e
Eric Kemp <eric.kemp@nasa.gov>
2025-09-18 14:53:30 -0400

    Code cleanup.

--- ldt/params/HYMAP/read_HYMAP_levee_height.F90
+++ ldt/params/HYMAP/read_HYMAP_levee_height.F90
@@ -20,15 +20,15 @@
 subroutine read_HYMAP_levee_height(n, array)
 ! !USES:
   use ESMF
+  use HYMAP_parmsMod
   use LDT_coreMod,      only : LDT_rc
+  use LDT_fileIOMod,    only : readLISdata
   use LDT_logMod,       only : LDT_logunit, LDT_getNextUnitNumber, &
           LDT_releaseUnitNumber, LDT_endrun
-  use LDT_fileIOMod,    only : readLISdata 
-  use HYMAP_parmsMod
 
   implicit none
-! !ARGUMENTS: 
 
+! !ARGUMENTS:
   integer,          intent(in) :: n
   real,          intent(inout) :: array(LDT_rc%lnc(n),LDT_rc%lnr(n),1)
 
@@ -39,16 +39,16 @@ subroutine read_HYMAP_levee_height(n, array)
   ftn = LDT_getNextUnitNumber()
 
   inquire(file=trim(HYMAP_struc(n)%leveeheightfile), exist=file_exists)
-  if(.not.file_exists) then 
+  if(.not.file_exists) then
      write(LDT_logunit,*) '[ERR] HYMAP levee height map, ',&
-           trim(HYMAP_struc(n)%leveeheightfile),', not found.'
+          trim(HYMAP_struc(n)%leveeheightfile),', not found.'
      write(LDT_logunit,*) 'Program stopping ...'
      call LDT_endrun
   endif
 
   open(ftn, file=trim(HYMAP_struc(n)%leveeheightfile), access='direct',&
        status='old', form="unformatted", convert="big_endian", recl=4)
-  
+
   call readLISdata(n, ftn, HYMAP_struc(n)%hymap_proj, &
        HYMAP_struc(n)%hymap_gridtransform, &
        HYMAP_struc(n)%hymapparms_gridDesc(:), 1, array)  ! 1 indicates 2D layer
