3039e035310dd2ca2783fbf30587bdd2dc9128da
James V. Geiger <james.v.geiger@nasa.gov>
2025-12-09 13:04:23 -0500

    Remove unused #include "LIS_NetCDF_inc.h"

--- lis/dataassim/obs/SWOT_WL/swotWLobs_module.F90
+++ lis/dataassim/obs/SWOT_WL/swotWLobs_module.F90
@@ -8,7 +8,6 @@
 ! All Rights Reserved.
 !-------------------------END NOTICE -- DO NOT EDIT-----------------------
 #include "LIS_misc.h"
-#include "LIS_NetCDF_inc.h"
 !BOP
 !
 ! !MODULE: swotWLobs_module
