bcab7bc3d04c6c96db3a4700ad68cccd5e64f0cb
Kristi R. Arsenault <kristi.r.arsenault@nasa.gov>
2026-02-09 11:43:22 -0500

    LISFV7.7 S2S final updates
     - Updated stand-alone Python scripts in the "ghis2s/s2s_app"
        directory that are used outside the main subsystem for checking
        files == pylint scores > 9.0.
     - Included fix to a s2smetric weekly script to account for issue
        during leap years.

--- lis/utils/usaf/S2S/ghis2s/s2s_app/verify_tiff.py
+++ lis/utils/usaf/S2S/ghis2s/s2s_app/verify_tiff.py
@@ -1,6 +1,7 @@
-'''
-This script computes median across all model ensembles for each lead month separately and compares against the S2S produced TIF file.
-'''
+"""
+This script computes median across all model ensembles for each lead month
+separately and compares against the S2S produced TIF file.
+"""
 
 #-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------
 # NASA Goddard Space Flight Center
@@ -21,6 +22,9 @@ import numpy as np
 import yaml
 
 def compute_median (anom, lead):
+    """
+    Compute the median across ensembles for a specific lead time.
+    """
     da_slice=[]
     for da in anom:
         da_slice.append(da.isel(time=lead))
@@ -28,7 +32,7 @@ def compute_median (anom, lead):
     return da_conc.median(dim = 'ens')
 
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--fcast_month', required=True, help='forecast start month 1-12')
     parser.add_argument('-y', '--fcast_year', required=True, help='forecast start year')
@@ -39,35 +43,51 @@ if __name__ == "__main__":
     with open(args.config_file, 'r', encoding="utf-8") as file:
         cfg = yaml.safe_load(file)
     sys.path.append(cfg['SETUP']['LISFDIR'] + '/lis/utils/usaf/s2s/')
-    from s2s_modules.shared import utils
-    
+    from ghis2s.shared import utils
+
     end_date = date(fcst_year, fcst_mon, 1) + relativedelta(months=int(cfg["EXP"]["lead_months"]))
-    s2smdir = './s2smetric/' + '{:04d}{:02d}'.format(fcst_year, fcst_mon) + '/metrics_cf/NOAHMP/'
+#    s2smdir = './s2smetric/' + '{:04d}{:02d}'.format(fcst_year, fcst_mon) + '/metrics_cf/NOAHMP/'
+    s2smdir = f"./s2smetric/{fcst_year:04d}{fcst_mon:02d}/metrics_cf/NOAHMP/"
     d0 = date(fcst_year, fcst_mon, 1)
-    
+
     for var in cfg["POST"]["metric_vars"]:
         print ("Comparing : ", var)
-        
+
         # read NC files
         anoms = []
         sanoms = []
-        print (s2smdir + 'PS.557WW_SC.U_DI.C_GP.LIS-S2S-*_GR.C0P25DEG_AR.GLOBAL_PA.S2SMETRICS_DD.{:04d}{:02d}01_FP.{:04d}{:02d}01-{}_DF.NC'.format(fcst_year, fcst_mon, fcst_year, fcst_mon, end_date.strftime("%Y%m%d")))
+        filename = (
+            f"{s2smdir}PS.557WW_SC.U_DI.C_GP.LIS-S2S-*_GR.C0P25DEG_AR.GLOBAL_"
+            f"PA.S2SMETRICS_DD.{fcst_year:04d}{fcst_mon:02d}01_"
+            f"FP.{fcst_year:04d}{fcst_mon:02d}01-{end_date:%Y%m%d}_DF.NC"
+        )
+        print(filename)
         for model in cfg["EXP"]["NMME_models"]:
-            ncfile = s2smdir + 'PS.557WW_SC.U_DI.C_GP.LIS-S2S-{}_GR.C0P25DEG_AR.GLOBAL_PA.S2SMETRICS_DD.{:04d}{:02d}01_FP.{:04d}{:02d}01-{}_DF.NC'.format(model.upper(), fcst_year, fcst_mon, fcst_year, fcst_mon, end_date.strftime("%Y%m%d")) 
+            ncfile = (
+                f"{s2smdir}PS.557WW_SC.U_DI.C_GP.LIS-S2S-{model.upper()}_"
+                f"GR.C0P25DEG_AR.GLOBAL_PA.S2SMETRICS_DD.{fcst_year:04d}{fcst_mon:02d}01_"
+                f"FP.{fcst_year:04d}{fcst_mon:02d}01-{end_date:%Y%m%d}_DF.NC"
+            )
             ncdata = xr.open_dataset(ncfile)
             anoms.append(ncdata[var + '_ANOM'])
             sanoms.append(ncdata[var + '_SANOM'])
             del ncdata
-    
+
         # read TIF files
-        for lead in range(0, int(cfg["EXP"]["lead_months"])):        
+        for lead in range(0, int(cfg["EXP"]["lead_months"])):
             d1 = date(fcst_year, fcst_mon, 1)  + relativedelta(months=lead)
             d2 = d1 + relativedelta(months=1)
-            tif_anom =  s2smdir + 'PS.557WW_SC.U_DI.C_GP.LIS-S2S-ANOM_GR.C0P25DEG_AR.GLOBAL_PA.{}_DD.{}_FP.{}-{}_DF.TIF'.format(var.upper(),d0.strftime("%Y%m%d"),d1.strftime("%Y%m%d"),d2.strftime("%Y%m%d"))
-            tif_sanom =  s2smdir + 'PS.557WW_SC.U_DI.C_GP.LIS-S2S-SANOM_GR.C0P25DEG_AR.GLOBAL_PA.{}_DD.{}_FP.{}-{}_DF.TIF'.format(var.upper(),d0.strftime("%Y%m%d"),d1.strftime("%Y%m%d"),d2.strftime("%Y%m%d"))
+            tif_anom = (
+                f"{s2smdir}PS.557WW_SC.U_DI.C_GP.LIS-S2S-ANOM_GR.C0P25DEG_AR.GLOBAL_"
+                f"PA.{var.upper()}_DD.{d0:%Y%m%d}_FP.{d1:%Y%m%d}-{d2:%Y%m%d}_DF.TIF"
+            )
+            tif_sanom = (
+                f"{s2smdir}PS.557WW_SC.U_DI.C_GP.LIS-S2S-SANOM_GR.C0P25DEG_AR.GLOBAL_"
+                f"PA.{var.upper()}_DD.{d0:%Y%m%d}_FP.{d1:%Y%m%d}-{d2:%Y%m%d}_DF.TIF"
+            )
             da_anom = utils.tiff_to_da(tif_anom)
             da_sanom = utils.tiff_to_da(tif_sanom)
-        
+
             anom_rev = da_anom.reindex(y=list(reversed(da_anom.y)))
             sanom_rev = da_sanom.reindex(y=list(reversed(da_sanom.y)))
 
@@ -76,8 +96,13 @@ if __name__ == "__main__":
             nc_med = compute_median (sanoms, lead)
             c2 = np.allclose(nc_med.values, sanom_rev.values, rtol=1.e-05,   equal_nan=True)
             print('LEAD : ', lead)
-            print ('PS.557WW_SC.U_DI.C_GP.LIS-S2S-ANOM_GR.C0P25DEG_AR.GLOBAL_PA.{}_DD.{}_FP.{}-{}_DF.TIF'.format(var.upper(),d0.strftime("%Y%m%d"),d1.strftime("%Y%m%d"),d2.strftime("%Y%m%d")), '  : ', c1)
-            print ('PS.557WW_SC.U_DI.C_GP.LIS-S2S-SANOM_GR.C0P25DEG_AR.GLOBAL_PA.{}_DD.{}_FP.{}-{}_DF.TIF'.format(var.upper(),d0.strftime("%Y%m%d"),d1.strftime("%Y%m%d"),d2.strftime("%Y%m%d")), '  : ', c2)
- 
-
-
+            filename = (
+                f"PS.557WW_SC.U_DI.C_GP.LIS-S2S-ANOM_GR.C0P25DEG_AR.GLOBAL_"
+                f"PA.{var.upper()}_DD.{d0:%Y%m%d}_FP.{d1:%Y%m%d}-{d2:%Y%m%d}_DF.TIF"
+            )
+            print(f"{filename} : {c1}")
+            filename_sanom = (
+                f"PS.557WW_SC.U_DI.C_GP.LIS-S2S-SANOM_GR.C0P25DEG_AR.GLOBAL_"
+                f"PA.{var.upper()}_DD.{d0:%Y%m%d}_FP.{d1:%Y%m%d}-{d2:%Y%m%d}_DF.TIF"
+            )
+            print(f"{filename_sanom} : {c2}")
