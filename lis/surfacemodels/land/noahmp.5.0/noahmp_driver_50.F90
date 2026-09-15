66f30c10f894c1ab50b6e43ced71cd578b000fbb
Eric Kemp <eric.kemp@nasa.gov>
2026-09-15 18:17:21 -0400

    Revised indentation, deleted dead code.

--- lis/surfacemodels/land/noahmp.5.0/noahmp_driver_50.F90
+++ lis/surfacemodels/land/noahmp.5.0/noahmp_driver_50.F90
@@ -7,65 +7,69 @@
 ! Administrator of the National Aeronautics and Space Administration.
 ! All Rights Reserved.
 !-------------------------END NOTICE -- DO NOT EDIT-----------------------
-! Oct 15 2018 Shugong Wang started for implementing Noah-MP 4.0.1 based on the version of 3.6  
-! Oct 15 2018 Zhuo Wang modifed for implementing Noah-MP 4.0.1 
 ! May 2023: Cenlin He modified for refactored Noah-MP v5 and later
 
-#undef LIS_NoahMP_TEST 
+#undef LIS_NoahMP_TEST
 ! !INTERFACE
-subroutine noahmp_driver_50(n, LISparam)   
+subroutine noahmp_driver_50(n, LISparam)
 
+  use LIS_constantsMod, only: LIS_CONST_PATH_LEN
   use LIS_coreMod, only    : LIS_rc
-  use LIS_logMod,  only    : LIS_logunit, LIS_endrun
+  use LIS_logMod,  only    : LIS_logunit, LIS_endrun, LIS_abort
+  use LisNoahmpParamType
   use LIS_timeMgrMod, only : LIS_date2time, LIS_tick
-  use NoahmpIOVarType
-  use LisNoahmpParamType 
   use NoahmpDriverMainMod
+  use NoahmpIOVarType
 
-  implicit none 
-  integer,                   intent(in)    :: n          ! nest id 
+  implicit none
+  integer,                   intent(in)    :: n          ! nest id
   type(LisNoahmpParam_type), intent(in)    :: LISparam   ! lis/noahmp parameter
 
-!--------------------------------------------------------------------------------
+  !----------------------------------------------------------------------------
   ! external function
   real, external      :: month_d_new
 
-  ! local variables 
+  ! local variables
   character(len=12)   :: nowdate
 
-  ! Added by David Mocko on 11/19/2018
   logical :: Bondvillecheck
   integer :: i
   integer :: locyr,locmo,locda,lochr,locmn,locss,locdoy
   real*8  :: loctime
   real    :: locgmt,change
 
+  character(len=LIS_CONST_PATH_LEN) :: abort_message(20)
+
   external :: calc_declin
 
   ! ---------------------------------------
-   NoahmpIO%xland(1,1) = 1
-   NoahmpIO%xice(1,1)  = 0
-   ! Fraction of grid determining seaice (from WRF and HRLDAS)
-   NoahmpIO%xice_threshold = 0.5
+  abort_message = ''
+
+  NoahmpIO%xland(1,1) = 1
+  NoahmpIO%xice(1,1)  = 0
+  ! Fraction of grid determining seaice (from WRF and HRLDAS)
+  NoahmpIO%xice_threshold = 0.5
 
 #ifndef WRF_HYDRO
-   NoahmpIO%sfcheadrt(1,1) = 0.0
+  NoahmpIO%sfcheadrt(1,1) = 0.0
 #endif
-! SR = frozen precip fraction.  For offline, it is set to zero.
-! If running coupled to WRF, then SR is set by the WRF model.
-   if (NoahmpIO%IOPT_SNF .ne. 4) then
-      NoahmpIO%SR(1,1) = 0.0
-   else
-      write(LIS_logunit,*) "[ERR] SR should be set by the WRF model."
-      write(LIS_logunit,*) "[ERR] Code needs fixing.  Stopping LIS."
-      call LIS_endrun
-   endif
-
-  !!!!! print all the options not supported in offline mode 
-  if (NoahmpIO%IOPT_SFC > 2) then
-     stop "(opt_sfc == 3) and (opt_sfc == 4) are not for offline use"
+  ! SR = frozen precip fraction.  For offline, it is set to zero.
+  ! If running coupled to WRF, then SR is set by the WRF model.
+  if (NoahmpIO%IOPT_SNF .ne. 4) then
+     NoahmpIO%SR(1,1) = 0.0
+  else
+     write(LIS_logunit,*) "[ERR] SR should be set by the WRF model."
+     write(LIS_logunit,*) "[ERR] Code needs fixing.  Stopping LIS."
+     call LIS_endrun
   endif
 
+!!!!! print all the options not supported in offline mode
+  if (NoahmpIO%IOPT_SFC > 2) then
+     !stop "(opt_sfc == 3) and (opt_sfc == 4) are not for offline use"
+     abort_message(1) = &
+          "(opt_sfc == 3) and (opt_sfc == 4) are not for offline use"
+     call LIS_abort(abort_message)
+  endif
 
   ! cosz, yearlen, and julian are calculated in subroutine calc_declin.
   ! Be careful here!!!, LIS uses GMT; the date for calc_declin_401 should
@@ -93,36 +97,44 @@ subroutine noahmp_driver_50(n, LISparam)
   locss = LIS_rc%ss
 
   call LIS_date2time(loctime,locdoy,locgmt,locyr,locmo,locda,lochr,locmn,locss)
-  call LIS_tick(loctime,locdoy,locgmt,locyr,locmo,locda,lochr,locmn,locss,change)
+  call LIS_tick(loctime,locdoy,locgmt,locyr,locmo,locda,lochr,locmn,locss, &
+       change)
 
   write(nowdate,'(I4.4,4I2.2)') locyr, locmo, locda, lochr, locmn
-  call calc_declin(nowdate(1:4)//"-"//nowdate(5:6)//"-"//nowdate(7:8)//"_"//nowdate(9:10)//":"//nowdate(11:12)//":00", &
-       NoahmpIO%xlat(1,1), NoahmpIO%xlon(1,1), NoahmpIO%coszen(1,1), NoahmpIO%yearlen, NoahmpIO%julian)
+  call calc_declin(nowdate(1:4)//"-"//nowdate(5:6)//"-"//nowdate(7:8)//"_"// &
+       nowdate(9:10)//":"//nowdate(11:12)//":00", &
+       NoahmpIO%xlat(1,1), NoahmpIO%xlon(1,1), NoahmpIO%coszen(1,1), &
+       NoahmpIO%yearlen, NoahmpIO%julian)
 
   read(nowdate(1:4), '(I4)') NoahmpIO%YR
 
-  if ((NoahmpIO%IOPT_DVEG .eq. 1).or.(NoahmpIO%IOPT_DVEG .eq. 6).or.(NoahmpIO%IOPT_DVEG .eq. 7)) then
-    ! with dveg_opt==1/6/7, shdfac is fed directly to fveg
-    NoahmpIO%vegfra(1,1) = month_d_new(NoahmpIO%shdfac_monthly(1,:,1), nowdate)
+  if ((NoahmpIO%IOPT_DVEG .eq. 1).or.(NoahmpIO%IOPT_DVEG .eq. 6).or. &
+       (NoahmpIO%IOPT_DVEG .eq. 7)) then
+     ! with dveg_opt==1/6/7, shdfac is fed directly to fveg
+     NoahmpIO%vegfra(1,1) = month_d_new(NoahmpIO%shdfac_monthly(1,:,1), &
+          nowdate)
   else
-    ! with dveg_opt==2/3/8, fveg is computed from lai and sai, and shdfac is unused
-    ! with dveg_opt==4/5/9, fveg is set to the maximum shdfac, and shdfac is unused
-    NoahmpIO%vegfra(1,1) = -1.E36   ! To match with HRLDAS initialization
+     ! with dveg_opt==2/3/8, fveg is computed from lai and sai, and shdfac is
+     ! unused
+     ! with dveg_opt==4/5/9, fveg is set to the maximum shdfac, and shdfac is
+     ! unused
+     NoahmpIO%vegfra(1,1) = -1.E36   ! To match with HRLDAS initialization
   endif
   NoahmpIO%gvfmax(1,1) = maxval(NoahmpIO%shdfac_monthly(1,:,1))
 
-  ! assign forcing variables 
+  ! assign forcing variables
   NoahmpIO%dz8w(1,2,1)     = NoahmpIO%dz8w(1,1,1)
-  NoahmpIO%T_PHY(1,2,1)    = NoahmpIO%T_PHY(1,1,1)  
+  NoahmpIO%T_PHY(1,2,1)    = NoahmpIO%T_PHY(1,1,1)
   NoahmpIO%P8W(1,2,1)      = NoahmpIO%P8W(1,1,1)
   NoahmpIO%QV_CURR(1,1,1)  = NoahmpIO%QV_CURR(1,1,1)/&
-                             (1.0 - NoahmpIO%QV_CURR(1,1,1)) ! Convert specific humidity to water vapor mixing ratio
+       (1.0 - NoahmpIO%QV_CURR(1,1,1)) ! Convert specific humidity to
+                                       ! water vapor mixing ratio
   NoahmpIO%QV_CURR(1,2,1)  = NoahmpIO%QV_CURR(1,1,1)
   NoahmpIO%U_PHY(1,2,1)    = NoahmpIO%U_PHY(1,1,1)
   NoahmpIO%V_PHY(1,2,1)    = NoahmpIO%V_PHY(1,1,1)
   NoahmpIO%QSFC(1,1)       = NoahmpIO%QV_CURR(1,1,1)
   NoahmpIO%SNOWBL(1,1)     = 0.0
-  NoahmpIO%SR(1,1)         = 0.0                              ! Will only use component if opt_snf=4
+  NoahmpIO%SR(1,1)         = 0.0 ! Will only use component if opt_snf=4
   NoahmpIO%RAINCV(1,1)     = 0.0
   NoahmpIO%RAINNCV(1,1)    = NoahmpIO%RAINBL(1,1)
   NoahmpIO%RAINSHV(1,1)    = 0.0
@@ -134,16 +146,18 @@ subroutine noahmp_driver_50(n, LISparam)
   ! and then pass back to WRF after the call to noahmp.
   NoahmpIO%z0(1,1)     = 0.0
   NoahmpIO%znt(1,1)    = 0.0
-  ! z0 and znt should be passed to WRF, if coupled. - dmm
+  ! z0 and znt should be passed to WRF, if coupled.
 
   ! Code from module_NoahMP_hrldas_driver.F.  Initial guess only.
-  if ((trim(LIS_rc%startcode).eq."coldstart").and.(NoahmpIO%itimestep.eq.1)) then
-     NoahmpIO%eahxy(1,1) = NoahmpIO%P8W(1,1,1) * (NoahmpIO%QV_CURR(1,1,1)/(0.622+NoahmpIO%QV_CURR(1,1,1)))
+  if ((trim(LIS_rc%startcode).eq."coldstart").and. &
+       (NoahmpIO%itimestep.eq.1)) then
+     NoahmpIO%eahxy(1,1) = NoahmpIO%P8W(1,1,1) * &
+          (NoahmpIO%QV_CURR(1,1,1)/(0.622+NoahmpIO%QV_CURR(1,1,1)))
      NoahmpIO%tahxy(1,1) = NoahmpIO%T_PHY(1,1,1)
      NoahmpIO%cmxy(1,1)  = 0.1
      NoahmpIO%chxy(1,1)  = 0.1
   endif
-  
+
   ! main NoahMP driver physics
   call NoahmpDriverMain(NoahmpIO,LISparam)
 
@@ -151,23 +165,26 @@ end subroutine noahmp_driver_50
 
 real function month_d_new(a12, nowdate) result (nowval)
   !
-  ! Given a set of 12 values, taken to be valid on the fifteenth of each month (Jan through Dec)
-  ! and a date in the form <YYYYMMDD[HHmmss]> ....
-  ! 
-  ! Return a value valid for the day given in <nowdate>, as an interpolation from the 12
-  ! monthly values.
+  ! Given a set of 12 values, taken to be valid on the fifteenth of each
+  ! month (Jan through Dec) and a date in the form <YYYYMMDD[HHmmss]> ....
+  ! Return a value valid for the day given in <nowdate>, as an interpolation
+  ! from the 12 monthly values.
   !
   use kwm_date_utilities_50
   implicit none
-  real, dimension(12), intent(in) :: a12 ! 12 monthly values, taken to be valid on the 15th of
-  !                                      ! the month
-  character(len=12), intent(in) :: nowdate ! Date, in the form <YYYYMMDD[HHmmss]>
+  real, dimension(12), intent(in) :: a12 ! 12 monthly values,
+                                         ! taken to be valid on the 15th of
+                                         ! the month
+  character(len=12), intent(in) :: nowdate ! Date, in the form
+                                           ! <YYYYMMDD[HHmmss]>
   integer :: nowy, nowm, nowd
   integer :: prevm, postm
   real    :: factor
-  integer, dimension(12) :: ndays = (/ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 /)
+  integer, dimension(12) :: ndays = (/ 31, 28, 31, 30, 31, 30, 31, 31, 30, &
+       31, 30, 31 /)
   !
-  ! Handle leap year by setting the number of days in February for the year in question.
+  ! Handle leap year by setting the number of days in February for the year
+  ! in question.
   !
   read(nowdate(1:8),'(I4,I2,I2)') nowy, nowm, nowd
   ndays(2) = nfeb(nowy)
@@ -192,10 +209,10 @@ real function month_d_new(a12, nowdate) result (nowval)
 
   nowval = a12(prevm)*(1.0-factor) + a12(postm)*factor
 
-end function month_d_new 
+end function month_d_new
 
 SUBROUTINE calc_declin ( nowdate, latitude, longitude, cosz, yearlen, julian)
-  use kwm_date_utilities_50 
+  use kwm_date_utilities_50
 !---------------------------------------------------------------------
   IMPLICIT NONE
 !---------------------------------------------------------------------
@@ -271,15 +288,14 @@ SUBROUTINE calc_declin ( nowdate, latitude, longitude, cosz, yearlen, julian)
   ARG=SINOB*SIN(SXLONG)
   DECLIN=ASIN(ARG)
 
-  TLOCTIM = REAL(IHOUR) + REAL(IMINUTE)/60.0 + REAL(ISECOND)/3600.0 + LONGITUDE/15.0 ! Local time in hours
+  TLOCTIM = REAL(IHOUR) + REAL(IMINUTE)/60.0 + REAL(ISECOND)/3600.0 + &
+       LONGITUDE/15.0 ! Local time in hours
   tloctim = AMOD(tloctim+24.0, 24.0)
   HRANG=15.*(TLOCTIM-12.)*DEGRAD
-  COSZ=SIN(LATITUDE*DEGRAD)*SIN(DECLIN)+COS(LATITUDE*DEGRAD)*COS(DECLIN)*COS(HRANG)
-  COSZ=MIN(COSZ,1.0);   !Added by kwH 3/1/16 to address floating point roundoff errors 
-  COSZ=MAX(COSZ,-1.0);  !
-
-!KWM   write(wrf_err_message,10)DECDEG/DEGRAD
-!KWM10 FORMAT(1X,'*** SOLAR DECLINATION ANGLE = ',F6.2,' DEGREES.',' ***')
+  COSZ=SIN(LATITUDE*DEGRAD)*SIN(DECLIN)+COS(LATITUDE*DEGRAD)*COS(DECLIN)* &
+       COS(HRANG)
+  COSZ=MIN(COSZ,1.0);   !Address floating point roundoff errors
+  COSZ=MAX(COSZ,-1.0);
 
 END SUBROUTINE calc_declin
 
