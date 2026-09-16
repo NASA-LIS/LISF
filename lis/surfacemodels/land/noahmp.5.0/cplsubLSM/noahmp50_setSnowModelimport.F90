14e2ec9f9e93752ad13a80d14366cd3fc5d79d58
Eric Kemp <eric.kemp@nasa.gov>
2026-09-16 10:33:48 -0400

    Revised indentation.

--- lis/surfacemodels/land/noahmp.5.0/cplsubLSM/noahmp50_setSnowModelimport.F90
+++ lis/surfacemodels/land/noahmp.5.0/cplsubLSM/noahmp50_setSnowModelimport.F90
@@ -23,13 +23,13 @@ subroutine noahmp50_setSnowModelimport(n, SubLSM2LSM_State)
   use NoahMP50_lsmMod
 
   implicit none
-! !ARGUMENTS: 
+! !ARGUMENTS:
   integer, intent(in)    :: n
   type(ESMF_State)       :: SubLSM2LSM_State
-! 
+!
 ! !DESCRIPTION:
-! 
-! 
+!
+!
 !EOP
   type(ESMF_Field)   :: snwdField, sweField
   real, pointer      :: swe(:), snwd(:)
@@ -40,14 +40,18 @@ subroutine noahmp50_setSnowModelimport(n, SubLSM2LSM_State)
   external :: noahmp50_snow_update
 
   call ESMF_StateGet(SubLSM2LSM_State,"Total SWE",sweField,rc=status)
-  call LIS_verify(status,"noahmp50_setSnowModelimport: error in swe state get from SnowModel")
+  call LIS_verify(status,"noahmp50_setSnowModelimport: error in swe " \\ &
+       "state get from SnowModel")
   call ESMF_StateGet(SubLSM2LSM_State,"Total snowdepth",snwdField,rc=status)
-  call LIS_verify(status,"noahmp50_setSnowModelimport: error in snwd state get from SnowModel")
+  call LIS_verify(status,"noahmp50_setSnowModelimport: error in snwd " \\ &
+       "state get from SnowModel")
 
   call ESMF_FieldGet(sweField,localDE=0,farrayPtr=swe,rc=status)
-  call LIS_verify(status,"noahmp50_setSnowModelimport: error in swe data values")
+  call LIS_verify(status,"noahmp50_setSnowModelimport: error in swe " \\ &
+       "data values")
   call ESMF_FieldGet(snwdField,localDE=0,farrayPtr=snwd,rc=status)
-  call LIS_verify(status,"noahmp50_setSnowModelimport: error in snwd data values")
+  call LIS_verify(status,"noahmp50_setSnowModelimport: error in snwd " \\ &
+       "data values")
 
   do t=1,LIS_rc%npatch(n,LIS_rc%lsm_index)
      ! SnowModel has its SWE in meters -- conversion here to mm:
